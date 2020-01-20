9726739d656d04016f3a402a3e6e4f6e2ae15edc
Martin Käck <martin.kack@slu.se>
2020-01-20 13:19:03 +0100

    Added unit test for importing Dyntaxa DwC-A.

--- Tests/Integration/SOS.Import.IntegrationTests/Taxonomy/ImportDyntaxaDwcaIntegrationTests.cs
+++ Tests/Integration/SOS.Import.IntegrationTests/Taxonomy/ImportDyntaxaDwcaIntegrationTests.cs
@@ -21,51 +21,6 @@ namespace SOS.Import.IntegrationTests.Taxonomy
     public class ImportDyntaxaDwcaIntegrationTests : TestBase
     {
         [Fact]
-        public async Task Reads_a_static_dyntaxa_dwca_file_and_verifies_multiple_taxon_properties()
-        {
-            //-----------------------------------------------------------------------------------------------------------
-            // Arrange
-            //-----------------------------------------------------------------------------------------------------------
-            TaxonServiceConfiguration taxonServiceConfiguration = new TaxonServiceConfiguration() { BaseAddress = "..." };
-            var taxonServiceProxyStub = TaxonServiceProxyStubFactory.Create(@"Resources\dyntaxa.custom.dwca.zip");
-            var sut = new TaxonService(taxonServiceProxyStub.Object, taxonServiceConfiguration, new NullLogger<TaxonService>());
-
-            //-----------------------------------------------------------------------------------------------------------
-            // Act
-            //-----------------------------------------------------------------------------------------------------------
-            IEnumerable<DarwinCoreTaxon> taxa = await sut.GetTaxaAsync();
-
-            //-----------------------------------------------------------------------------------------------------------
-            // Assert - Beech species DarwinCore properties
-            //-----------------------------------------------------------------------------------------------------------
-            //var beechSpeciesTaxon = taxa.Single(t => t.TaxonID == "urn:lsid:dyntaxa.se:Taxon:220778");
-            var beechSpeciesTaxon = taxa.Single(t => t.Id == 220778);
-            beechSpeciesTaxon.TaxonRank.Should().Be("species");
-            beechSpeciesTaxon.Kingdom.Should().Be("Plantae");
-            beechSpeciesTaxon.ScientificName.Should().Be("Fagus sylvatica");
-            beechSpeciesTaxon.ScientificNameAuthorship.Should().Be("L.");
-            beechSpeciesTaxon.VernacularName.Should().Be("bok");
-            beechSpeciesTaxon.NomenclaturalStatus.Should().Be("valid");
-            beechSpeciesTaxon.TaxonomicStatus.Should().Be("accepted");
-            beechSpeciesTaxon.DynamicProperties.DyntaxaTaxonId.Should().Be(220778);
-            beechSpeciesTaxon.HigherClassification.Should().Be("Biota | Plantae | Viridiplantae | Streptophyta | Embryophyta | Tracheophyta | Euphyllophytina | Spermatophytae | Angiospermae | Magnoliopsida | Fagales | Fagaceae | Fagus");
-
-            //-----------------------------------------------------------------------------------------------------------
-            // Assert - Beech (bok) species has one recommended swedish vernacular name,
-            //          two swedish synonyms and one english synonym
-            //-----------------------------------------------------------------------------------------------------------
-            var expectedBeechSpeciesTaxonVernacularNames = CreateExpectedBeechSpeciesTaxonVernacularNames();
-            beechSpeciesTaxon.DynamicProperties.VernacularNames.Should().BeEquivalentTo(expectedBeechSpeciesTaxonVernacularNames);
-
-            //-----------------------------------------------------------------------------------------------------------
-            // Assert - Beech (bok) genus has one main parent and one secondary parent.
-            //-----------------------------------------------------------------------------------------------------------
-            var beechGenusTaxon = taxa.Single(t => t.TaxonID == "urn:lsid:dyntaxa.se:Taxon:1006037");
-            beechGenusTaxon.DynamicProperties.ParentDyntaxaTaxonId.Should().Be(2002757); // Family - Fagaceae (bokväxter)
-            beechGenusTaxon.DynamicProperties.SecondaryParentDyntaxaTaxonIds.Should().BeEquivalentTo(new List<int> { 6005257 }); // Organism group - Hardwood forest trees (ädellövträd)
-        }
-
-        [Fact]
         public async Task Reads_a_static_dyntaxa_dwca_file_and_stores_the_taxa_in_MongoDb()
         {
             //-----------------------------------------------------------------------------------------------------------
@@ -144,19 +99,6 @@ namespace SOS.Import.IntegrationTests.Taxonomy
             // Assert
             //-----------------------------------------------------------------------------------------------------------
             tree.Root.ScientificName.Should().Be("Biota");
-        }
-
-        private List<TaxonVernacularName> CreateExpectedBeechSpeciesTaxonVernacularNames()
-        {
-            List<TaxonVernacularName> beechVernacularNames = new List<TaxonVernacularName>
-            {
-                new TaxonVernacularName { Name = "bok", Language = "sv", CountryCode = "SE", IsPreferredName = true },
-                new TaxonVernacularName { Name = "vanlig bok", Language = "sv", CountryCode = "SE", IsPreferredName = false },
-                new TaxonVernacularName { Name = "rödbok", Language = "sv", CountryCode = "SE", IsPreferredName = false },
-                new TaxonVernacularName { Name = "Beech", Language = "en", CountryCode = "GB", IsPreferredName = false }
-            };
-
-            return beechVernacularNames;
         }
     }
 }
--- Tests/Unit/SOS.Import.UnitTests/Services/TaxonServiceTests.cs
+++ Tests/Unit/SOS.Import.UnitTests/Services/TaxonServiceTests.cs
@@ -1,12 +1,16 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using FluentAssertions;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
 using Moq;
 using SOS.Import.Services;
 using SOS.Import.Services.Interfaces;
+using SOS.Import.UnitTests.TestHelpers.Factories;
 using SOS.Lib.Configuration.Import;
+using SOS.Lib.Models.Processed.DarwinCore;
 using Xunit;
 
 namespace SOS.Import.UnitTests.Services
@@ -108,5 +112,64 @@ namespace SOS.Import.UnitTests.Services
 
             result.Should().BeNull();
         }
+
+        [Fact]
+        public async Task Parse_a_static_dyntaxa_dwca_file_and_verifies_multiple_taxon_properties()
+        {
+            //-----------------------------------------------------------------------------------------------------------
+            // Arrange
+            //-----------------------------------------------------------------------------------------------------------
+            TaxonServiceConfiguration taxonServiceConfiguration = new TaxonServiceConfiguration { BaseAddress = "..." };
+            var taxonServiceProxyStub = TaxonServiceProxyStubFactory.Create(@"Resources\dyntaxa.custom.dwca.zip");
+            var sut = new TaxonService(taxonServiceProxyStub.Object, taxonServiceConfiguration, new NullLogger<TaxonService>());
+
+            //-----------------------------------------------------------------------------------------------------------
+            // Act
+            //-----------------------------------------------------------------------------------------------------------
+            IEnumerable<DarwinCoreTaxon> taxa = await sut.GetTaxaAsync();
+
+            //-----------------------------------------------------------------------------------------------------------
+            // Assert - Beech species DarwinCore properties
+            //-----------------------------------------------------------------------------------------------------------
+            var beechSpeciesTaxon = taxa.Single(t => t.Id == 220778);
+            beechSpeciesTaxon.TaxonRank.Should().Be("species");
+            beechSpeciesTaxon.Kingdom.Should().Be("Plantae");
+            beechSpeciesTaxon.ScientificName.Should().Be("Fagus sylvatica");
+            beechSpeciesTaxon.ScientificNameAuthorship.Should().Be("L.");
+            beechSpeciesTaxon.VernacularName.Should().Be("bok");
+            beechSpeciesTaxon.NomenclaturalStatus.Should().Be("valid");
+            beechSpeciesTaxon.TaxonomicStatus.Should().Be("accepted");
+            beechSpeciesTaxon.DynamicProperties.DyntaxaTaxonId.Should().Be(220778);
+            beechSpeciesTaxon.HigherClassification.Should().Be("Biota | Plantae | Viridiplantae | Streptophyta | Embryophyta | Tracheophyta | Euphyllophytina | Spermatophytae | Angiospermae | Magnoliopsida | Fagales | Fagaceae | Fagus");
+
+            //-----------------------------------------------------------------------------------------------------------
+            // Assert - Beech (bok) species has one recommended swedish vernacular name,
+            //          two swedish synonyms and one english synonym
+            //-----------------------------------------------------------------------------------------------------------
+            var expectedBeechSpeciesTaxonVernacularNames = CreateExpectedBeechSpeciesTaxonVernacularNames();
+            beechSpeciesTaxon.DynamicProperties.VernacularNames.Should().BeEquivalentTo(expectedBeechSpeciesTaxonVernacularNames);
+
+            //-----------------------------------------------------------------------------------------------------------
+            // Assert - Beech (bok) genus has one main parent and one secondary parent.
+            //-----------------------------------------------------------------------------------------------------------
+            var beechGenusTaxon = taxa.Single(t => t.Id == 1006037);
+            beechGenusTaxon.DynamicProperties.ParentDyntaxaTaxonId.Should().Be(2002757); // Family - Fagaceae (bokväxter)
+            beechGenusTaxon.DynamicProperties.SecondaryParentDyntaxaTaxonIds.Should().BeEquivalentTo(new List<int> { 6005257 }); // Organism group - Hardwood forest trees (ädellövträd)
+        }
+
+
+        private List<TaxonVernacularName> CreateExpectedBeechSpeciesTaxonVernacularNames()
+        {
+            List<TaxonVernacularName> beechVernacularNames = new List<TaxonVernacularName>
+            {
+                new TaxonVernacularName { Name = "bok", Language = "sv", CountryCode = "SE", IsPreferredName = true },
+                new TaxonVernacularName { Name = "vanlig bok", Language = "sv", CountryCode = "SE", IsPreferredName = false },
+                new TaxonVernacularName { Name = "rödbok", Language = "sv", CountryCode = "SE", IsPreferredName = false },
+                new TaxonVernacularName { Name = "Beech", Language = "en", CountryCode = "GB", IsPreferredName = false }
+            };
+
+            return beechVernacularNames;
+        }
+
     }
 }
