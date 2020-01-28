3404371bbb96e041f1e80ed8175014021621ec6b
Mats Lindgren <mats.lindgren@slu.se>
2020-01-28 15:18:30 +0100

    Misc config

--- Src/SOS.Hangfire.UI/Startup.cs
+++ Src/SOS.Hangfire.UI/Startup.cs
@@ -81,7 +81,6 @@ namespace SOS.Hangfire.UI
                             Description = "An API to handle various processing jobs" //,
                             //TermsOfService = "None"
                         });
-                    options.DescribeAllEnumsAsStrings();
                     // Set the comments path for the Swagger JSON and UI.
                     var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
                     var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
