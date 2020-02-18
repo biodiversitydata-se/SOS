e272d556782350677a22a7d3e087b2e56a82dcba
Martin Käck <martin.kack@slu.se>
2020-02-18 12:42:44 +0100

    Refactoring MessagePack usage in tests.

--- Tests/TestHelpers/SOS.TestHelpers/Helpers/MessagePackHelper.cs
+++ Tests/TestHelpers/SOS.TestHelpers/Helpers/MessagePackHelper.cs
@@ -10,6 +10,16 @@ namespace SOS.TestHelpers.Helpers
 {
     public static class MessagePackHelper
     {
+        public static T CreateFromMessagePackFile<T>(string fileName)
+        {
+            string assemblyPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            var filePath = Path.Combine(assemblyPath, fileName);
+            var bytes = File.ReadAllBytes(filePath);
+            var options = ContractlessStandardResolver.Options.WithCompression(MessagePackCompression.Lz4BlockArray);
+            var objects = MessagePackSerializer.Deserialize<T>(bytes, options);
+            return objects;
+        }
+
         public static List<T> CreateListFromMessagePackFile<T>(string fileName)
         {
             string assemblyPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
