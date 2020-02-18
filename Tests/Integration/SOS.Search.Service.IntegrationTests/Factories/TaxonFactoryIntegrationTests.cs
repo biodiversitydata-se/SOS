7963fff0d852c1eba650dd149e575286b5aabc17
Martin Käck <martin.kack@slu.se>
2020-02-18 11:12:06 +0100

    Fixed TaxonFactoryIntegrationTests

--- Tests/Integration/SOS.Search.Service.IntegrationTests/Factories/TaxonFactoryIntegrationTests.cs
+++ Tests/Integration/SOS.Search.Service.IntegrationTests/Factories/TaxonFactoryIntegrationTests.cs
@@ -8,41 +8,52 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging.Abstractions;
 using Microsoft.Extensions.Options;
 using MongoDB.Driver;
+using SOS.Export.Repositories;
 using SOS.Lib.Configuration.Export;
 using SOS.Lib.Configuration.Shared;
 using SOS.Lib.Models.Processed.Sighting;
+using SOS.Search.Service.Database;
 using SOS.Search.Service.Factories;
 using SOS.Search.Service.Repositories;
 using Xunit;
+using ProcessedTaxonRepository = SOS.Search.Service.Repositories.ProcessedTaxonRepository;
 
 namespace SOS.Search.Service.IntegrationTests.Factories
 {
     public class TaxonFactoryIntegrationTests : TestBase
     {
-        //[Fact]
-        //public void A_taxon_tree_is_created_from_taxon_data_in_sos_processed_db()
-        //{
-        //    //-----------------------------------------------------------------------------------------------------------
-        //    // Arrange
-        //    //-----------------------------------------------------------------------------------------------------------
-        //    var mongoDbConfiguration = GetMongoDbConfiguration();
-        //    var mongoClient = new MongoClient(mongoDbConfiguration.GetMongoDbSettings());
-        //    var options = Options.Create(mongoDbConfiguration);
-        //    var processedTaxonRepository = new ProcessedTaxonRepository(
-        //        mongoClient,
-        //        options,
-        //        new NullLogger<BaseRepository<ProcessedTaxon, int>>());
-        //    var taxonFactory = new TaxonFactory(processedTaxonRepository, new NullLogger<TaxonFactory>());
+        [Fact]
+        public void A_taxon_tree_is_created_from_sos_processed_db()
+        {
+            //-----------------------------------------------------------------------------------------------------------
+            // Arrange
+            //-----------------------------------------------------------------------------------------------------------
+            var taxonFactory = CreateTaxonFactory();
 
-        //    //-----------------------------------------------------------------------------------------------------------
-        //    // Act
-        //    //-----------------------------------------------------------------------------------------------------------
-        //    var taxonTree = taxonFactory.TaxonTree;
+            //-----------------------------------------------------------------------------------------------------------
+            // Act
+            //-----------------------------------------------------------------------------------------------------------
+            var taxonTree = taxonFactory.TaxonTree;
 
-        //    //-----------------------------------------------------------------------------------------------------------
-        //    // Assert
-        //    //-----------------------------------------------------------------------------------------------------------
-        //    taxonTree.Root.ScientificName.Should().Be("Biota");
-        //}
+            //-----------------------------------------------------------------------------------------------------------
+            // Assert
+            //-----------------------------------------------------------------------------------------------------------
+            taxonTree.Root.ScientificName.Should().Be("Biota");
+        }
+
+        private TaxonFactory CreateTaxonFactory()
+        {
+            var mongoDbConfiguration = GetMongoDbConfiguration();
+            var processClient = new ProcessClient(
+                mongoDbConfiguration.GetMongoDbSettings(),
+                mongoDbConfiguration.DatabaseName,
+                mongoDbConfiguration.BatchSize);
+
+            var processedTaxonRepository = new ProcessedTaxonRepository(
+                processClient,
+                new NullLogger<ProcessBaseRepository<ProcessedTaxon, int>>());
+            var taxonFactory = new TaxonFactory(processedTaxonRepository, new NullLogger<TaxonFactory>());
+            return taxonFactory;
+        }
     }
 }
