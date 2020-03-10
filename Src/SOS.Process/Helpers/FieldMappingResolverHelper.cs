d6ece0d04277a2bcd7ab4aac93c577e8ec230378
Mats Lindgren <mats.lindgren@slu.se>
2020-03-10 14:45:55 +0100

    Config fix

--- Src/SOS.Process/Helpers/FieldMappingResolverHelper.cs
+++ Src/SOS.Process/Helpers/FieldMappingResolverHelper.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using SOS.Lib.Configuration.Process;
 using SOS.Lib.Constants;
--- Src/SOS.lib/Configuration/Process/FieldMappingConfiguration.cs
+++ Src/SOS.lib/Configuration/Process/FieldMappingConfiguration.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
+﻿
 namespace SOS.Lib.Configuration.Process
 {
     /// <summary>
