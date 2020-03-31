25f2129c6866d92499068d5533060f61c5fe908c
Martin Käck <martin.kack@slu.se>
2020-03-31 09:03:33 +0200

    Add setting for processing observations sequentially for easier debugging purpose

--- Src/SOS.lib/Configuration/Process/ProcessConfiguration.cs
+++ Src/SOS.lib/Configuration/Process/ProcessConfiguration.cs
@@ -8,6 +8,11 @@ namespace SOS.Lib.Configuration.Process
     public class ProcessConfiguration
     {
         /// <summary>
+        /// Decides whether parallell processing should be used.
+        /// </summary>
+        public bool ParallelProcessing { get; set; }
+        
+        /// <summary>
         /// No of threads to run in parallel
         /// </summary>
         public int NoOfThreads { get; set; }
