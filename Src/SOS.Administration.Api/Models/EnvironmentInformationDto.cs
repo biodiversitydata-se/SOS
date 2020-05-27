3a93604bee0109100e4e4823078551860ea9b352
Martin Käck <martin.kack@slu.se>
2020-05-27 10:33:52 +0200

    Add environment controller.

---
+++ Src/SOS.Administration.Api/Models/EnvironmentInformationDto.cs
@@ -0,0 +1,28 @@
+﻿namespace SOS.Administration.Api.Models
+{
+    /// <summary>
+    /// Environment information
+    /// </summary>
+    public class EnvironmentInformationDto
+    {
+        /// <summary>
+        /// Type of environment (Local, Dev, ST, AT, Prod)
+        /// </summary>
+        public string EnvironmentType { get; set; }
+
+        /// <summary>
+        /// Name of the server hosting the application/service
+        /// </summary>
+        public string HostingServerName { get; set; }
+
+        /// <summary>
+        /// Server operating system.
+        /// </summary>
+        public string OsPlatform { get; set; }
+        
+        /// <summary>
+        /// .Net version
+        /// </summary>
+        public string AspDotnetVersion { get; set; }
+    }
+}
