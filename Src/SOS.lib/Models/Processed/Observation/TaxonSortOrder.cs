117756355a8aa4ecba112ac9b2391b61009a19df
Mikael Jönsson <mikael.jonsson@slu.se>
2020-06-02 08:36:01 +0200

    Added new sort orders from the dwc-file instead of the service

---
+++ Src/SOS.lib/Models/Processed/Observation/TaxonSortOrder.cs
@@ -0,0 +1,12 @@
+﻿namespace SOS.Lib.Models.Processed.Observation
+{
+    /// <summary>
+    /// Taxon sort order record relation.
+    /// </summary>    
+    public class TaxonSortOrder<T>
+    { 
+        public T TaxonId { get; set; }
+        public int? SortOrder { get; set; }        
+    }
+
+}
