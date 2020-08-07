e47b54a40dfb8cf941faa7791ec84820df51776a
Johan Iverås <johan.iveras@slu.se>
2020-08-07 13:30:16 +0200

    Can now search for multiple projects.

--- Src/SOS.lib/Models/Search/SearchFilterInternal.cs
+++ Src/SOS.lib/Models/Search/SearchFilterInternal.cs
@@ -45,7 +45,7 @@ namespace SOS.Lib.Models.Search
         }
 
         public int? UserId { get; set; }
-        public int? ProjectId { get; set; }
+        public List<int> ProjectIds { get; set; }
         public bool IncludeRealCount { get; set; }
         public List<double> BoundingBox { get; set; }
         /// <summary>
