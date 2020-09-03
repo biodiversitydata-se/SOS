accf4cc5ea593633538e77fc2576365b5ada5552
Martin Käck <martin.kack@slu.se>
2020-09-03 15:45:03 +0200

    Add descriptions to DwC-A data validation report.

--- Src/SOS.lib/Models/DataValidation/DwcaDataValidationReport.cs
+++ Src/SOS.lib/Models/DataValidation/DwcaDataValidationReport.cs
@@ -6,14 +6,29 @@ namespace SOS.Lib.Models.DataValidation
     public class DwcaDataValidationReport<TVerbatim, TProcessed>
     {
         public object Settings { get; set; }
+        public DwcaDatavalidationReportDescription Descriptions => new DwcaDatavalidationReportDescription();
         public DwcaDataValidationReportSummary Summary { get; set; }
         public List<ValidObservationTuple<TVerbatim, TProcessed>> ValidObservations { get; set; }
         public List<InvalidObservationTuple<TVerbatim>> InvalidObservations { get; set; }
         public List<DistinctValuesSummary> DictionaryValues { get; set; }
     }
 
+    public class DwcaDatavalidationReportDescription
+    {
+        public string TotalNumberOfObservationsInFile => "The total number of observations in the DwC-A file.";
+        public string NrObservationsProcessed => "The number of observations that have been processed to create this report.";
+        public string NrValidObservations => "The number of observations that could be processed without validation errors.";
+        public string NrInvalidObservations => "The number of observations that could not be processed due to validation errors. For example, that no matching taxon in Dyntaxa was found, or that the observation is outside Sweden's borders.";
+        public string Remarks => "Remarks on data that is missing in the file, which could be included to improve the quality of the processing.";
+        public string ObservationDefects => "Validation error information.";
+        public string ValidObservations => "A listing of valid observations (default is 100). For each observation, the following is shown: VerbatimObservation = raw data from the DwC-A CSV files. ProcessedObservation = this is what the observation looks like in the SOS database after it has been processed. DwcExport = this is what the observation looks like when the processed observation is exported to DwC.";
+        public string InvalidObservations => "A listing of invalid observations (default is 100). For each observation, the following is shown: VerbatimObservation = raw data from the DwC-A CSV files. ProcessedObservationDefects = a list of defects.";
+        public string DictionaryValues => "Some fields have a predefined dictionary in SOS, for example the fields BasisOfRecord, Gender, LifeStage, ValidationStatus, OccurrenceStatus & Municipality. This is a summary of what values that could be mapped to those dictionary entries.";
+    }
+
     public class DwcaDataValidationReportSummary
     {
+        public string ReportCreatedDate { get; set; }
         public int TotalNumberOfObservationsInFile { get; set; }
         public int NrObservationsProcessed { get; set; }
         public int NrValidObservations { get; set; }
