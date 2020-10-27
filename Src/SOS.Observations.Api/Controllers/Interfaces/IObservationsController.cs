b2d90fb6abbd736cf8bad9538296b6554e46004f
Martin Käck <martin.kack@slu.se>
2020-10-27 11:41:22 +0100

    Remove "/Observations/TermDictionary" endpoint

--- Src/SOS.Observations.Api/Controllers/Interfaces/IObservationsController.cs
+++ Src/SOS.Observations.Api/Controllers/Interfaces/IObservationsController.cs
@@ -40,12 +40,5 @@ namespace SOS.Observations.Api.Controllers.Interfaces
         /// <param name="providerId"></param>
         /// <returns></returns>
         Task<IActionResult> GetLatestModifiedDateForProviderAsync(int providerId);
-
-        /// <summary>
-        ///     Term dictionary are used for properties with multiple acceptable fixed values. E.g gender can have the values: male, female...
-        ///     Term dictionary also describe the different possible query parameters available in searches.
-        /// </summary>
-        /// <returns>List of term dicionaries.</returns>
-        Task<IActionResult> GetFieldMappingAsync();
     }
 }
