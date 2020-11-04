9d55c553800472bae07ef0fdb6eb733ffe230c10
Mats Lindgren <mats.lindgren@slu.se>
2020-11-04 14:55:27 +0100

    Added attempts to MVM harvest

--- Src/SOS.Import/Services/MvmObservationService.cs
+++ Src/SOS.Import/Services/MvmObservationService.cs
@@ -14,26 +14,7 @@ namespace SOS.Import.Services
         private readonly MvmServiceConfiguration _mvmServiceConfiguration;
         private readonly ISpeciesObservationChangeService _speciesObservationChangeServiceClient;
 
-        /// <summary>
-        ///     Constructor
-        /// </summary>
-        /// <param name="speciesObservationChangeServiceClient"></param>
-        /// <param name="mvmServiceConfiguration"></param>
-        /// <param name="logger"></param>
-        public MvmObservationService(
-            ISpeciesObservationChangeService speciesObservationChangeServiceClient,
-            MvmServiceConfiguration mvmServiceConfiguration,
-            ILogger<MvmObservationService> logger)
-        {
-            _speciesObservationChangeServiceClient = speciesObservationChangeServiceClient ??
-                                                     throw new ArgumentNullException(
-                                                         nameof(speciesObservationChangeServiceClient));
-            _mvmServiceConfiguration = mvmServiceConfiguration ??
-                                       throw new ArgumentNullException(nameof(mvmServiceConfiguration));
-            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
-        }
-
-        public async Task<Tuple<long, IEnumerable<WebSpeciesObservation>>> GetAsync(long getFromId)
+        private async Task<Tuple<long, IEnumerable<WebSpeciesObservation>>> GetAsync(long getFromId, byte attempt)
         {
             try
             {
@@ -58,11 +39,41 @@ namespace SOS.Import.Services
                 return new Tuple<long, IEnumerable<WebSpeciesObservation>>(result.MaxChangeId,
                     result.CreatedSpeciesObservations);
             }
-            catch(Exception e)
+            catch (Exception e)
             {
-                _logger.LogError($"Failed to get MVM observations from id: {getFromId}", e);
-               return new Tuple<long, IEnumerable<WebSpeciesObservation>>(0L, null);
+                _logger.LogError(e, $"Failed to get MVM observations from id: {getFromId}, attempt: {attempt}");
+                // Give it up to tree attempts to get the data
+                if (attempt < 4)
+                {
+                    return await GetAsync(getFromId, ++attempt);
+                }
+
+                return new Tuple<long, IEnumerable<WebSpeciesObservation>>(0L, null);
             }
+        }
+
+        /// <summary>
+        ///     Constructor
+        /// </summary>
+        /// <param name="speciesObservationChangeServiceClient"></param>
+        /// <param name="mvmServiceConfiguration"></param>
+        /// <param name="logger"></param>
+        public MvmObservationService(
+            ISpeciesObservationChangeService speciesObservationChangeServiceClient,
+            MvmServiceConfiguration mvmServiceConfiguration,
+            ILogger<MvmObservationService> logger)
+        {
+            _speciesObservationChangeServiceClient = speciesObservationChangeServiceClient ??
+                                                     throw new ArgumentNullException(
+                                                         nameof(speciesObservationChangeServiceClient));
+            _mvmServiceConfiguration = mvmServiceConfiguration ??
+                                       throw new ArgumentNullException(nameof(mvmServiceConfiguration));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        public async Task<Tuple<long, IEnumerable<WebSpeciesObservation>>> GetAsync(long getFromId)
+        {
+            return await GetAsync(getFromId, 1);
         }
     }
 }
