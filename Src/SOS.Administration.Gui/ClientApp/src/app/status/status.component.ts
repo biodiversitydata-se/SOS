a6d01be2b26d9d898f11d26c475298ffdef5247f
Mikael Jönsson <mikael.jonsson@slu.se>
2020-11-19 10:48:01 +0100

    Added status view for admin gui, add environment-variables to startup for admin

---
+++ Src/SOS.Administration.Gui/ClientApp/src/app/status/status.component.ts
@@ -0,0 +1,101 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, Inject, OnInit } from '@angular/core';
+import { format, parseISO, formatDistanceStrict, formatDuration, intervalToDuration } from 'date-fns'
+
+
+function dateFormatter(params) {
+  if (params.value) {
+    return format(parseISO(params.value), 'yyyy-MM-dd HH:mm:ss');
+  }
+  else {
+    return '';
+  }
+}
+
+@Component({
+  selector: 'app-status',
+  templateUrl: './status.component.html',
+  styleUrls: ['./status.component.scss']
+})
+export class StatusComponent implements OnInit {
+  http: HttpClient;
+  baseUrl: string;
+  statuses = [];
+  processInfo: ProcessInfo;
+
+  processColumnDefs = [
+    {
+      field: 'processStatus', sortable: true, filter: true, resizable: true, cellRenderer: function (params) {
+        return params.value == "Success" ? '<svg height="20" width="30"><circle cx="10" cy="10" r="10" fill="green" /></svg><span>' + params.value + '</span>' :
+          '<svg height="20" width="30"><circle cx="10" cy="10" r="10" fill="red" /></svg><span>' + params.value + '</span>'
+      }
+    },
+    {
+      field: 'harvestStatus', sortable: true, filter: true, resizable: true, cellRenderer: function (params) {
+        return params.value == "Success" ? '<svg height="20" width="30"><circle cx="10" cy="10" r="10" fill="green" /></svg><span>' + params.value + '</span>' :
+          '<svg height="20" width="30"><circle cx="10" cy="10" r="10" fill="red" /></svg><span>' + params.value + '</span>'
+      }
+    },    
+    { field: 'dataProviderIdentifier', sortable: true, filter: true, resizable: true },
+    { field: 'processCount', sortable: true, filter: true, resizable: true },
+    { field: 'processStart', sortable: true, filter: true, resizable: true, valueFormatter: dateFormatter },
+    { field: 'processEnd', sortable: true, filter: true, resizable: true, valueFormatter: dateFormatter },   
+    { field: 'harvestCount', sortable: true, filter: true, resizable: true },
+    { field: 'harvestStart', sortable: true, filter: true, resizable: true, valueFormatter: dateFormatter },
+    { field: 'harvestEnd', sortable: true, filter: true, resizable: true, valueFormatter: dateFormatter },    
+  ];
+
+  processRowData = [
+  ];
+  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+    this.http = http;
+    this.baseUrl = baseUrl
+  }
+
+  ngOnInit() {
+    this.statuses = [];
+    this.http.get<ProcessInfo>(this.baseUrl + 'statusinfo/process').subscribe(result => {
+      this.processInfo = result;          
+    }, error => console.error(error));
+  }
+  formatDate(param) {
+    return format(parseISO(param), 'yyyy-MM-dd HH:mm:ss')
+  }
+  getTimeTaken(start, end) {
+    var startDate = parseISO(start);
+    var endDate = parseISO(end);
+    var duration = intervalToDuration({
+      start: startDate,
+      end: endDate
+    })
+    return formatDuration(duration);
+  }
+}
+class Provider {
+  dataProviderId: number;
+  dataProviderIdentifier: string
+  processCount: number;
+  processStart: Date;
+  processEnd: Date;
+  rocessStatus: string;
+  harvestCount: number;
+  harvestStart: Date;
+  harvestEnd: Date;
+  harvestStatus: string;
+}
+class ProcessInfo {
+  id: string;
+  count: string;
+  start: Date;
+  end: Date;  
+  status: string;
+  providersInfo: Provider[];
+}
+class HarvestInfo {
+  id: string;
+  count: string;
+  start: Date;
+  end: Date;
+  dataLastModified: Date;
+  status: string;
+}
---
+++ Src/SOS.Administration.Gui/Controllers/StatusInfoController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver;
+using SOS.Administration.Gui.Models;
+
+namespace SOS.Administration.Gui.Controllers
+{
+    [BsonIgnoreExtraElements]
+    public class HarvestInfo
+    {
+        public string Id { get; set; }
+        
+        public int Count{ get; set; }
+
+        public DateTime? DataLastModified { get; set; }
+        public DateTime End { get; set; }
+        public DateTime Start { get; set; }
+        public string Status { get; set; }
+    }
+    [BsonIgnoreExtraElements]
+    public class ProcessInfo
+    {
+        [BsonIgnoreExtraElements]
+        public class Provider
+        {
+            public int? DataProviderId { get; set; }
+            public string DataProviderIdentifier { get; set; }
+            public DateTime? ProcessEnd { get; set; }
+            public DateTime? ProcessStart { get; set; }
+            public string ProcessStatus { get; set; }
+            public int? ProcessCount { get; set; }
+            public DateTime? HarvestEnd { get; set; }
+            public DateTime? HarvestStart { get; set; }
+            public string HarvestStatus { get; set; }
+            public int? HarvestCount { get; set; }
+        }
+        public string Id { get; set; }
+
+        public int Count { get; set; }
+        public DateTime End { get; set; }
+        public DateTime Start { get; set; }
+        public string Status { get; set; }
+        public IEnumerable<Provider> ProvidersInfo{ get; set; }
+    }
+    [ApiController]
+    [Route("[controller]")]
+    public class StatusInfoController : ControllerBase
+    {       
+        private readonly ILogger<StatusInfoController> _logger;
+        private MongoClient _client;
+
+        public StatusInfoController(ILogger<StatusInfoController> logger, IInvalidObservationsDatabaseSettings invalidObservationsDatabaseSettings)
+        {
+            _logger = logger;
+            _client = new MongoClient(invalidObservationsDatabaseSettings.ConnectionString);
+        }
+
+        [HttpGet]
+        [Route("harvest")]
+        public IEnumerable<HarvestInfo> GetHarvestInfo()
+        {
+            var database = _client.GetDatabase("sos-harvest");            
+            var collection = database.GetCollection<HarvestInfo>("HarvestInfo");
+            var providers = collection.Find(new BsonDocument());
+            return providers.ToList();          
+       }
+        [HttpGet]
+        [Route("process")]
+        public IEnumerable<ProcessInfo> GetProcessInfo()
+        {
+            var database = _client.GetDatabase("sos");
+            var collection = database.GetCollection<ProcessInfo>("ProcessInfo");
+            var providers = collection.Find(new BsonDocument());
+            return providers.SortByDescending(p=>p.End).ToList();
+        }
+    }
+}
