cb5b9198550fb6c83640f135bc94e8c1788dd25e
Mikael Jönsson <mikael.jonsson@slu.se>
2020-11-20 13:12:21 +0100

    Move the map into its own component in admin gui

---
+++ Src/SOS.Administration.Gui/ClientApp/src/app/invalid-map/invalid-map.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { InvalidMapComponent } from './invalid-map.component';
+
+describe('InvalidMapComponent', () => {
+  let component: InvalidMapComponent;
+  let fixture: ComponentFixture<InvalidMapComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ InvalidMapComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(InvalidMapComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ Src/SOS.Administration.Gui/ClientApp/src/app/invalid-map/invalid-map.component.ts
@@ -0,0 +1,66 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, Inject, Input, OnInit } from '@angular/core';
+import { icon, latLng, marker, tileLayer } from 'leaflet';
+import { InvalidLocation } from '../models/invalidlocation';
+
+@Component({
+  selector: 'app-invalid-map',
+  templateUrl: './invalid-map.component.html',
+  styleUrls: ['./invalid-map.component.scss']
+})
+export class InvalidMapComponent implements OnInit {
+  @Input()
+  get dataSetId(): string { return this._dataSetId }
+  set dataSetId(id: string) {
+    this._dataSetId = id;
+    this.updateMap();
+  }
+  private _dataSetId: string = "0";
+  @Input()
+  get instance(): string { return this._instance }
+  set instance(id: string) {
+    this._instance = id;
+    this.updateMap();
+  }
+  private _instance: string = "0";
+  public options = {
+    layers: [
+      tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: 'OpenStreetMap' })
+    ],
+    zoom: 5,
+    center: latLng(46.879966, -121.726909)
+  };
+  http: HttpClient;
+  baseUrl: string;
+  loadingData: boolean = false;
+  markerClusterOptions = {};
+  markerClusterData = [];
+  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+    this.http = http;
+    this.baseUrl = baseUrl;
+  }
+
+  ngOnInit() {
+    this.updateMap();   
+  }
+
+  updateMap() {
+    this.markerClusterData = [];
+    this.loadingData = true;
+    this.http.get<InvalidLocation[]>(this.baseUrl + 'invalidobservations?dataSetId=' + this._dataSetId + "&instanceId=" + this._instance).subscribe(result => {
+      this.markerClusterData = result.map(function (val, index) {
+        var description = '<span>Id:' + val.occurrenceId + '</span><br/><span>DatasetName:' + val.dataSetName + '</span><br/><span>DatasetId:' + val.dataSetId + '</span>'
+        var m = marker([val.lat, val.lon], {
+          icon: icon({
+            iconSize: [25, 41],
+            iconAnchor: [13, 41],
+            iconUrl: 'assets/marker-icon.png',
+            shadowUrl: 'assets/marker-shadow.png'
+          })
+        }).bindPopup(description);
+        return m;
+      });
+      this.loadingData = false;
+    }, error => console.error(error));
+  }
+}
