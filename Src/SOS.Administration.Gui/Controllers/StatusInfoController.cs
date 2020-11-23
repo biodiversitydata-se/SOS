f04045fba846a8d273bf00953b09c93e5ce1a0a3
Mikael Jönsson <mikael.jonsson@slu.se>
2020-11-23 11:02:20 +0100

    Add -st configurations for admin.gui

--- Src/SOS.Administration.Gui/Controllers/StatusInfoController.cs
+++ Src/SOS.Administration.Gui/Controllers/StatusInfoController.cs
@@ -23,11 +23,18 @@ namespace SOS.Administration.Gui.Controllers
         private readonly ILogger<StatusInfoController> _logger;
         private MongoClient _mongoClient;
         private ElasticClient _elasticClient;
+        private string _mongoSuffix = "";
+        private MongoDbConfiguration _mongoConfiguration;
 
         public StatusInfoController(ILogger<StatusInfoController> logger, IOptionsMonitor<MongoDbConfiguration> mongoDbSettings, IOptionsMonitor<ElasticSearchConfiguration> elasticConfiguration)
         {
             _logger = logger;
-            _mongoClient = new MongoClient(mongoDbSettings.CurrentValue.GetMongoDbSettings());            
+            _mongoClient = new MongoClient(mongoDbSettings.CurrentValue.GetMongoDbSettings());
+            _mongoConfiguration = mongoDbSettings.CurrentValue;
+            if (_mongoConfiguration.DatabaseName.EndsWith("-st"))
+            {
+                _mongoSuffix = "-st";
+            }
             _elasticClient = elasticConfiguration.CurrentValue.GetClient();
 
         }
@@ -36,7 +43,7 @@ namespace SOS.Administration.Gui.Controllers
         [Route("harvest")]
         public IEnumerable<HarvestInfoDto> GetHarvestInfo()
         {
-            var database = _mongoClient.GetDatabase("sos-harvest");            
+            var database = _mongoClient.GetDatabase("sos-harvest" + _mongoSuffix);            
             var collection = database.GetCollection<HarvestInfoDto>("HarvestInfo");
             var providers = collection.Find(new BsonDocument());
             return providers.ToList();          
@@ -45,7 +52,7 @@ namespace SOS.Administration.Gui.Controllers
         [Route("process")]
         public IEnumerable<ProcessInfoDto> GetProcessInfo()
         {
-            var database = _mongoClient.GetDatabase("sos");
+            var database = _mongoClient.GetDatabase(_mongoConfiguration.DatabaseName);
             var collection = database.GetCollection<ProcessInfoDto>("ProcessInfo");
             var providers = collection.Find(new BsonDocument());
             return providers.SortByDescending(p=>p.End).ToList();
@@ -54,7 +61,7 @@ namespace SOS.Administration.Gui.Controllers
         [Route("activeinstance")]
         public ActiveInstanceInfoDto GetActiveInstance()
         {
-            var database = _mongoClient.GetDatabase("sos");
+            var database = _mongoClient.GetDatabase(_mongoConfiguration.DatabaseName);
             var collection = database.GetCollection<ActiveInstanceInfoDto>("ProcessedConfiguration");
             var instance = collection.Find(new BsonDocument()).ToList();
             return instance.FirstOrDefault();
@@ -63,7 +70,7 @@ namespace SOS.Administration.Gui.Controllers
         [Route("processing")]
         public IEnumerable<HangfireJobDto> GetProcessing()
         {
-            var database = _mongoClient.GetDatabase("sos-hangfire");
+            var database = _mongoClient.GetDatabase("sos-hangfire" + _mongoSuffix);
             var collection = database.GetCollection<HangfireJobDto>("hangfire.jobGraph");
             var filter = Builders<HangfireJobDto>.Filter.Eq(p => p.StateName, "Processing");
             var jobs = collection.Find(filter).ToList();
@@ -80,14 +87,17 @@ namespace SOS.Administration.Gui.Controllers
                 var allocations = new List<SearchIndexInfoDto.AllocationInfo>();
                 foreach(var record in allocation.Records)
                 {
-                    allocations.Add(new SearchIndexInfoDto.AllocationInfo()
+                    if (int.TryParse(record.DiskPercent, out int percentage))
                     {
-                        Node = record.Node,
-                        DiskAvailable = record.DiskAvailable,
-                        DiskTotal = record.DiskTotal,
-                        DiskUsed = record.DiskUsed,
-                        Percentage = int.Parse(record.DiskPercent)
-                    });
+                        allocations.Add(new SearchIndexInfoDto.AllocationInfo()
+                        {
+                            Node = record.Node,
+                            DiskAvailable = record.DiskAvailable,
+                            DiskTotal = record.DiskTotal,
+                            DiskUsed = record.DiskUsed,
+                            Percentage = percentage
+                        });
+                    }
                 }
                 info.Allocations = allocations;
             }
