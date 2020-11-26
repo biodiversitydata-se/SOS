aac8d915d917270dcb20cf415b1413ecce38aea1
Mats Lindgren <mats.lindgren@slu.se>
2020-11-26 08:54:23 +0100

    Virtual Herbarium position bug

---
+++ Src/SOS.lib/Cache/AreaCache.cs
@@ -0,0 +1,16 @@
+﻿using SOS.Lib.Cache.Interfaces;
+
+namespace SOS.Lib.Cache
+{
+    /// <summary>
+    /// Area cache
+    /// </summary>
+    public class AreaCache : ICache
+    {
+        /// <inheritdoc />
+        public void Release()
+        {
+
+        }
+    }
+}
