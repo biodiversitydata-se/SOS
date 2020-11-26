c374ca8653cc854256a532e95e3654280ff28a33
Mikael Jönsson <mikael.jonsson@slu.se>
2020-11-26 12:45:10 +0100

    Added initial statistics view to admin gui

---
+++ Src/SOS.Administration.Gui/ClientApp/src/app/performance-chart/performance-chart.component.ts
@@ -0,0 +1,104 @@
+import { Component, Inject, OnInit, ViewChild } from '@angular/core';
+import { ChartDataSets, ChartOptions, ChartType } from 'chart.js';
+import { Color, BaseChartDirective, Label } from 'ng2-charts';
+import * as pluginAnnotations from 'chartjs-plugin-annotation';
+import { HttpClient } from '@angular/common/http';
+import { FunctionalTest } from '../models/functionaltest';
+
+@Component({
+  selector: 'app-performance-chart',
+  templateUrl: './performance-chart.component.html',
+  styleUrls: ['./performance-chart.component.scss']
+})
+export class PerformanceChartComponent implements OnInit {
+  public lineChartData: ChartDataSets[] = [ ];  
+  public lineChartOptions: (ChartOptions & { annotation: any }) = {
+    responsive: true,
+    scales: {
+      // We use this empty structure as a placeholder for dynamic theming.
+      xAxes: [{
+        type: 'time'        
+      }],
+      yAxes: [
+        {
+          id: 'y-axis-0',
+          position: 'left',
+        }
+      ]
+    },
+    annotation: {
+      annotations: [
+        {
+          type: 'line',
+          mode: 'vertical',
+          scaleID: 'x-axis-0',
+          value: 'March',
+          borderColor: 'orange',
+          borderWidth: 2,
+          label: {
+            enabled: true,
+            fontColor: 'orange',
+            content: 'LineAnno'
+          }
+        },
+      ],
+    },
+  };
+  public lineChartColors: Color[] = [
+    { // grey
+      backgroundColor: 'rgba(148,159,177,0.2)',
+      borderColor: 'rgba(148,159,177,1)',
+      pointBackgroundColor: 'rgba(148,159,177,1)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+    },
+    { // dark grey
+      backgroundColor: 'rgba(77,83,96,0.2)',
+      borderColor: 'rgba(77,83,96,1)',
+      pointBackgroundColor: 'rgba(77,83,96,1)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(77,83,96,1)'
+    },
+    { // red
+      backgroundColor: 'rgba(255,0,0,0.3)',
+      borderColor: 'red',
+      pointBackgroundColor: 'rgba(148,159,177,1)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+    }
+  ];
+  public lineChartLegend = true;
+  public lineChartType: ChartType = 'line';
+  public lineChartPlugins = [pluginAnnotations];
+
+  @ViewChild(BaseChartDirective, { static: true }) chart: BaseChartDirective;
+    veryDifferentColors: string[];
+
+  constructor(public http: HttpClient, @Inject('BASE_URL') public baseUrl: string) {
+    
+    this.veryDifferentColors = ["#000000", "#00FF00", "#0000FF", "#FF0000", "#01FFFE", "#FFA6FE", "#FFDB66", "#006401", "#010067", "#95003A", "#007DB5", "#FF00F6", "#FFEEE8", "#774D00", "#90FB92", "#0076FF", "#D5FF00", "#FF937E", "#6A826C", "#FF029D", "#FE8900", "#7A4782", "#7E2DD2", "#85A900", "#FF0056", "#A42400", "#00AE7E", "#683D3B", "#BDC6FF", "#263400", "#BDD393", "#00B917", "#9E008E", "#001544", "#C28C9F", "#FF74A3", "#01D0FF", "#004754", "#E56FFE", "#788231", "#0E4CA1", "#91D0CB", "#BE9970", "#968AE8", "#BB8800", "#43002C", "#DEFF74", "#00FFC6", "#FFE502", "#620E00", "#008F9C", "#98FF52", "#7544B1", "#B500FF", "#00FF78", "#FF6E41", "#005F39", "#6B6882", "#5FAD4E", "#A75740", "#A5FFD2", "#FFB167", "#009BFF", "#E85EBE"];
+  }
+
+  ngOnInit(): void {    
+    this.http.get<FunctionalTest[]>(this.baseUrl + 'tests').subscribe(tests => {
+      this.lineChartData = [];
+      for (let test of tests) {
+        this.http.get<PerformanceData[]>(this.baseUrl + 'performance/' + test.id).subscribe(result => {
+
+          this.lineChartData.push({ data: result.map(p => { return { x: Date.parse(p.timestamp), y: p.timeTakenMs } }), label: test.description, backgroundColor: this.idxToColor(test.id) });
+        })
+      }
+    })
+  }
+  idxToColor(idx) {         
+    return this.veryDifferentColors[idx] + "77";
+  }
+}
+class PerformanceData {
+  timestamp: string;
+  timeTakenMs: number;
+  eventCount: number;
+}
---
+++ Src/SOS.Administration.Gui/Controllers/PerformanceController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Nest;
+using SOS.Administration.Gui.Models;
+using SOS.Lib.Configuration.Shared;
+
+namespace SOS.Administration.Gui.Controllers
+{
+    public class PerformanceData
+    {
+        public DateTime Timestamp { get; set; }
+        public double? TimeTakenMs { get; set; }
+        public long? EventCount { get; set; }
+    }
+    [Route("[controller]")]
+    [ApiController]
+    public class PerformanceController : ControllerBase
+    {
+        private readonly ElasticClient _elasticClient;
+        private readonly string _indexName;
+
+        public PerformanceController(IOptionsMonitor<ElasticSearchConfiguration> elasticConfiguration)
+        {
+            _elasticClient = elasticConfiguration.CurrentValue.GetClient();
+            _indexName = elasticConfiguration.CurrentValue.IndexPrefix + "-performance-data";
+        }
+        [HttpGet]
+        [Route("{testId}")]
+        public async Task<IEnumerable<PerformanceData>> GetPerformanceData(int testId)
+        {
+            var result = await _elasticClient.SearchAsync<PerformanceResult>(p => p.
+                Index(_indexName).
+                Query(q =>q.Term(t=>t.Field("testId").Value(testId))).
+                Aggregations(agg => agg.
+                                DateHistogram("date_histo",
+                                    e=>e.
+                                        Field("timestamp").
+                                        FixedInterval(new Time(TimeSpan.FromMinutes(5))).
+                                        Aggregations(agg2=>agg2.
+                                            Sum("the_sum", su=>su.
+                                                Field("timeTakenMs"))
+                                            .MovingFunction("the_movavg",mf => mf
+                                                .Window(100)
+                                                .BucketsPath("the_sum")
+                                                .Script("MovingFunctions.unweightedAvg(values)")
+                                                )))));
+            var histogram = result.Aggregations.DateHistogram("date_histo");
+            var data = new List<PerformanceData>();
+            foreach(var item in histogram.Buckets)
+            {
+                var performanceData = new PerformanceData()
+                {
+                    Timestamp = item.Date,
+                    EventCount = item.DocCount
+                };
+                if (item.ContainsKey("the_movavg"))
+                {
+                    var nested = (ValueAggregate)item["the_movavg"];
+                    performanceData.TimeTakenMs = nested.Value;
+                }
+                data.Add(performanceData);
+            }
+            return data;
+        }
+    }
+}
