55cc3d6a415f4d8a86313bd2365e628b500f952a
Mats Lindgren <mats.lindgren@slu.se>
2020-11-26 08:54:23 +0100

    Virtual Herbarium position bug

---
+++ Src/SOS.lib/Cache/Interfaces/ICache.cs
@@ -0,0 +1,13 @@
+﻿namespace SOS.Lib.Cache.Interfaces
+{
+    /// <summary>
+    /// Cache interface
+    /// </summary>
+    public interface ICache
+    {
+        /// <summary>
+        /// Release current cache
+        /// </summary>
+        void Release();
+    }
+}
