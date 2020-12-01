cd616f82ce3e313f649efa5e7ee5f704697feb46
Mikael Jönsson <mikael.jonsson@slu.se>
2020-12-01 16:07:14 +0100

    Move the default date filter to the top to make sure swagger shows it as default

--- Src/SOS.Observations.Api/Dtos/Filter/DateFilterDto.cs
+++ Src/SOS.Observations.Api/Dtos/Filter/DateFilterDto.cs
@@ -3,13 +3,21 @@
 namespace SOS.Observations.Api.Dtos.Filter
 {
     /// <summary>
+    /// OverlappingStartDateAndEndDate, Start or EndDate of the observation must be within the specified interval    
+    /// BetweenStartDateAndEndDate, Start and EndDate of the observation must be within the specified interval    
     /// OnlyStartDate, Only StartDate of the observation must be within the specified interval            
     /// OnlyEndDate, Only EndDate of the observation must be within the specified interval    
-    /// BetweenStartDateAndEndDate, Start and EndDate of the observation must be within the specified interval    
-    /// OverlappingStartDateAndEndDate, Start or EndDate of the observation must be within the specified interval    
     /// </summary>
     public enum DateFilterTypeDto
     {
+        /// <summary>
+        /// Start or EndDate of the observation must be within the specified interval
+        /// </summary>
+        OverlappingStartDateAndEndDate,
+        /// <summary>
+        /// Start and EndDate of the observation must be within the specified interval
+        /// </summary>
+        BetweenStartDateAndEndDate,
         /// <summary>
         /// Only StartDate of the observation must be within the specified interval
         /// </summary>
@@ -17,15 +25,7 @@ namespace SOS.Observations.Api.Dtos.Filter
         /// <summary>
         /// Only EndDate of the observation must be within the specified interval
         /// </summary>
-        OnlyEndDate,
-        /// <summary>
-        /// Start and EndDate of the observation must be within the specified interval
-        /// </summary>
-        BetweenStartDateAndEndDate,
-        /// <summary>
-        /// Start or EndDate of the observation must be within the specified interval
-        /// </summary>
-        OverlappingStartDateAndEndDate
+        OnlyEndDate       
     }
     /// <summary>
     /// Date filter.
