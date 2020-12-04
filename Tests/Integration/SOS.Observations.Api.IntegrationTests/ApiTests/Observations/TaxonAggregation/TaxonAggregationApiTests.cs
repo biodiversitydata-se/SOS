f032752fa8feb63364d439239da6a85dcf08313f
Martin Käck <martin.kack@slu.se>
2020-12-04 16:49:39 +0100

    Add API tests.

---
+++ Tests/Integration/SOS.Observations.Api.IntegrationTests/ApiTests/Observations/TaxonAggregation/TaxonAggregationApiTests.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentAssertions;
+using SOS.Observations.Api.Dtos.Filter;
+using SOS.Observations.Api.IntegrationTests.Fixtures;
+using Xunit;
+
+namespace SOS.Observations.Api.IntegrationTests.ApiTests.Observations.TaxonAggregation
+{
+
+    [Collection("Api test collection")]
+    public class TaxonAggregationApiTests : TestBase
+    {
+        private readonly ApiTestFixture _fixture;
+
+        public TaxonAggregationApiTests(ApiTestFixture apiTestFixture)
+        {
+            _fixture = apiTestFixture;
+        }
+
+        [Fact]
+        [Trait("Catgory", "ApiTest")]
+        public async Task TaxonAggregation()
+        {
+            //-----------------------------------------------------------------------------------------------------------
+            // Arrange
+            //-----------------------------------------------------------------------------------------------------------
+            SearchFilterDto searchFilter = new SearchFilterDto
+            {
+                Date = new DateFilterDto()
+                {
+                    StartDate = new DateTime(1990, 1, 31, 07, 59, 46),
+                    EndDate = new DateTime(2020, 1, 31, 07, 59, 46)
+                },
+                OnlyValidated = false,
+                OccurrenceStatus = OccurrenceStatusFilterValuesDto.Present
+            };
+
+            //-----------------------------------------------------------------------------------------------------------
+            // Act
+            //-----------------------------------------------------------------------------------------------------------
+            var result = await _fixture.SosApiClient.SearchSosTaxonAggregation(searchFilter, 100, 0);
+
+            //-----------------------------------------------------------------------------------------------------------
+            // Assert
+            //-----------------------------------------------------------------------------------------------------------
+            result.TotalCount.Should().BeGreaterThan(30000, "There are observations on more than 30 000 taxa");
+            result.Records.First().ObservationCount.Should().BeGreaterThan(100000,
+                "The taxon with most observations has more than 100 000 observations");
+        }
+
+        [Fact]
+        [Trait("Catgory", "ApiTest")]
+        public async Task TaxonAggregation_with_boundingbox()
+        {
+            //-----------------------------------------------------------------------------------------------------------
+            // Arrange
+            //-----------------------------------------------------------------------------------------------------------
+            SearchFilterDto searchFilter = new SearchFilterDto
+            {
+                Date = new DateFilterDto()
+                {
+                    StartDate = new DateTime(1990, 1, 31, 07, 59, 46),
+                    EndDate = new DateTime(2020, 1, 31, 07, 59, 46)
+                },
+                OnlyValidated = false,
+                OccurrenceStatus = OccurrenceStatusFilterValuesDto.Present
+            };
+
+            //-----------------------------------------------------------------------------------------------------------
+            // Act
+            //-----------------------------------------------------------------------------------------------------------
+            var result = await _fixture.SosApiClient.SearchSosTaxonAggregation(
+                searchFilter, 
+                500, 
+                0,
+                17.9296875, 
+                59.355596110016315, 
+                18.28125, 
+                59.17592824927137);
+
+            //-----------------------------------------------------------------------------------------------------------
+            // Assert
+            //-----------------------------------------------------------------------------------------------------------
+            result.TotalCount.Should().BeGreaterThan(8000, "There are observations on more than 8 000 taxa inside the bounding box");
+            result.Records.First().ObservationCount.Should().BeGreaterThan(2500,
+                "The taxon with most observations inside the bounding box has more than 2 500 observations");
+        }
+    }
+}
---
+++ Tests/Integration/SOS.Observations.Api.IntegrationTests/Configuration/ApiTestConfiguration.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SOS.Observations.Api.IntegrationTests.Configuration
+{
+    public class ApiTestConfiguration
+    {
+        public string ApiUrl { get; set; }
+    }
+}
