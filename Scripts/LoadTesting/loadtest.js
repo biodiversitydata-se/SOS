66b028830a243e482c4efb6e6f748e2df38f1585
Mikael Jönsson <mikael.jonsson@slu.se>
2020-12-10 08:10:27 +0100

    Make load tests shorter to allow easier testing

--- Scripts/LoadTesting/loadtest.js
+++ Scripts/LoadTesting/loadtest.js
@@ -4,7 +4,7 @@ import http from "k6/http";
 export let options = {
   stages: [
     { duration: '1m', target: 200 }, // simulate ramp-up of traffic from 1 to 100 users over 1 minute.
-    { duration: '12m', target: 200 }, // stay at 100 users for 10 minutes
+    { duration: '1m', target: 200 }, // stay at 100 users for 10 minutes
     { duration: '1m', target: 0 }, // ramp-down to 0 users
   ],
   thresholds: {
