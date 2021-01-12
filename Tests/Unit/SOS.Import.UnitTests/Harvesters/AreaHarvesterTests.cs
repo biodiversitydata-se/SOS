bf6a6ca7be6a237d883c2c19126eea8f72bfa8a6
Martin Käck <martin.kack@slu.se>
2021-01-12 10:54:01 +0100

    Fix unit test: HarvestAreasAsyncSuccess()

--- Tests/Unit/SOS.Import.UnitTests/Harvesters/AreaHarvesterTests.cs
+++ Tests/Unit/SOS.Import.UnitTests/Harvesters/AreaHarvesterTests.cs
@@ -76,7 +76,7 @@ namespace SOS.Import.UnitTests.Harvesters
             // Arrange
             //-----------------------------------------------------------------------------------------------------------
             _areaRepositoryMock.Setup(mdr => mdr.GetAsync())
-                .ReturnsAsync(new[] {new AreaEntity {FeatureId = "1", Name = "Sverige"}});
+                .ReturnsAsync(new[] {new AreaEntity {FeatureId = "1", Name = "Sverige", PolygonWKT = "POLYGON ((30 10, 40 40, 20 40, 10 20, 30 10))" } });
 
             _areaProcessedRepository.Setup(tr => tr.DeleteCollectionAsync())
                 .ReturnsAsync(true);
