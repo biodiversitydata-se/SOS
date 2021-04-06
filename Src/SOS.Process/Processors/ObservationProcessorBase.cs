ee34d1ddc9b0646fd192fafe586685f9b4a53feb
Martin Käck <martin.kack@slu.se>
2021-04-06 14:40:39 +0200

    Fix DwC-A export vocabulary resolve issue

--- Src/SOS.Process/Processors/ObservationProcessorBase.cs
+++ Src/SOS.Process/Processors/ObservationProcessorBase.cs
@@ -202,7 +202,7 @@ namespace SOS.Process.Processors
         {
 
             Logger.LogDebug($"Start writing {dataProvider.Identifier} CSV ({batchId})");
-            vocabularyValueResolver.ResolveVocabularyMappedValues(processedObservations, Cultures.en_GB);
+            vocabularyValueResolver.ResolveVocabularyMappedValues(processedObservations, Cultures.en_GB, true);
             var success = await dwcArchiveFileWriterCoordinator.WriteObservations(processedObservations, dataProvider, batchId);
 
             Logger.LogDebug($"Finish writing {dataProvider.Identifier} CSV ({batchId}) - {success}");
