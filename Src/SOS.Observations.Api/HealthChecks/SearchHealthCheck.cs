7fc062104111082ed5c7c8a1256b5479b18b4a98
Mats Lindgren <mats.lindgren@slu.se>
2021-04-15 09:57:39 +0200

    HealthCheck first draft

---
+++ Src/SOS.Observations.Api/HealthChecks/SearchHealthCheck.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace SOS.Observations.Api.HealthChecks
+{
+    public class SearchHealthCheck : IHealthCheck
+    {
+        public Task<HealthCheckResult> CheckHealthAsync(
+        HealthCheckContext context,
+        CancellationToken cancellationToken = default(CancellationToken))
+        {
+            var healthCheckResultHealthy = false;
+
+            if (healthCheckResultHealthy)
+            {
+                return Task.FromResult(
+                    HealthCheckResult.Healthy("A healthy result."));
+            }
+
+            return Task.FromResult(
+                new HealthCheckResult(HealthStatus.Degraded,
+                    "An unhealthy result."));
+
+            return Task.FromResult(
+                new HealthCheckResult(context.Registration.FailureStatus,
+                "An unhealthy result."));
+        }
+    }
+}
