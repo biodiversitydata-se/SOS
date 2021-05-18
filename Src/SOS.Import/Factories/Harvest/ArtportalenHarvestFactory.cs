11c7baaad2085795f0b39d8747d225bc71f0149d
Mikael Jönsson <mikael.jonsson@slu.se>
2021-05-18 14:50:12 +0200

    Remove refetching of projects in incremental harvesting

--- Src/SOS.Import/Factories/Harvest/ArtportalenHarvestFactory.cs
+++ Src/SOS.Import/Factories/Harvest/ArtportalenHarvestFactory.cs
@@ -223,12 +223,13 @@ namespace SOS.Import.Factories.Harvest
             {
                 return null;
             }
+            /*
             //if we are doing incremental harvesting then we need to make sure to fetch any new projects that may have been created
             if (live)
             {
                 var projectEntities = (await _projectRepository.GetProjectsAsync(live))?.ToArray();
                 _artportalenMetadataContainer.UpdateProjects(projectEntities);
-            }
+            }*/
             // Cast a projects to verbatim
             var sightingsProjects = new Dictionary<int, IDictionary<int, Project>>();
 
