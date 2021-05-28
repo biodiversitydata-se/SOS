955130260df69b805ae0e589baebd40f39bc5637
Mats Lindgren <mats.lindgren@slu.se>
2021-05-28 14:04:03 +0200

    EML as xml

--- Src/SOS.Observations.Api/Controllers/DataProvidersController.cs
+++ Src/SOS.Observations.Api/Controllers/DataProvidersController.cs
@@ -2,12 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using SOS.Observations.Api.Controllers.Interfaces;
 using SOS.Observations.Api.Dtos;
 using SOS.Observations.Api.Managers.Interfaces;
+using Newtonsoft.Json;
+using SOS.Lib.Extensions;
 
 namespace SOS.Observations.Api.Controllers
 {
@@ -87,20 +91,48 @@ namespace SOS.Observations.Api.Controllers
 
         /// <inheritdoc/>
         [HttpGet("{providerId}/EML")]
-        [ProducesResponseType(typeof(IEnumerable<DateTime>), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(string), (int)HttpStatusCode.OK)]
         [ProducesResponseType((int)HttpStatusCode.NoContent)]
         [ProducesResponseType((int)HttpStatusCode.InternalServerError)]
         public async Task<IActionResult> GetEML([FromRoute] int providerId)
         {
             try
             {
+                var returnJson = false;
+                if (Request.Headers.ContainsKey("Accept"))
+                {
+                    returnJson = Request.Headers["Accept"].Equals("application/json");
+                }
+
                 var fileData = await _dataProviderManager.GetEmlFileAsync(providerId);
 
                 if (!fileData?.Any() ?? true)
                 {
                     return new StatusCodeResult((int)HttpStatusCode.NoContent);
                 }
-                return File(fileData, "application/xml", $"Provider-{providerId}.xml");
+
+                if (!returnJson)
+                {
+                    return new ContentResult
+                    {
+                        ContentType = "application/xml",
+                        Content = Encoding.UTF8.GetString(fileData),
+                        StatusCode = 200
+                    };
+                }
+
+                var xDocument = await fileData.ToXmlAsync();
+                var doc = new XmlDocument();
+                doc.LoadXml(xDocument.ToString());
+                var json = JsonConvert.SerializeXmlNode(doc, Newtonsoft.Json.Formatting.Indented);
+
+                return new ContentResult
+                {
+                    ContentType = "application/json",
+                    Content = json,
+                    StatusCode = 200
+                };
+                
             }
             catch (Exception e)
             {
