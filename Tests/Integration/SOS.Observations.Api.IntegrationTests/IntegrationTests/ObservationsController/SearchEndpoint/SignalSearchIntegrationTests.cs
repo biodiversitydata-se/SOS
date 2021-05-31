ccc09f77141005090b6f658b4c5e945add6e0860
Martin Käck <martin.kack@slu.se>
2021-05-31 15:00:57 +0200

    Add signal search test

--- Tests/Integration/SOS.Observations.Api.IntegrationTests/IntegrationTests/ObservationsController/SearchEndpoint/SignalSearchIntegrationTests.cs
+++ Tests/Integration/SOS.Observations.Api.IntegrationTests/IntegrationTests/ObservationsController/SearchEndpoint/SignalSearchIntegrationTests.cs
@@ -10,6 +10,7 @@ using SOS.Observations.Api.Dtos;
 using SOS.Observations.Api.Dtos.Filter;
 using SOS.Observations.Api.IntegrationTests.Extensions;
 using SOS.Observations.Api.IntegrationTests.Fixtures;
+using SOS.TestHelpers.Helpers.Builders;
 using Xunit;
 
 namespace SOS.Observations.Api.IntegrationTests.IntegrationTests.ObservationsController.SearchEndpoint
@@ -26,15 +27,19 @@ namespace SOS.Observations.Api.IntegrationTests.IntegrationTests.ObservationsCon
 
         [Fact]
         [Trait("Category", "ApiIntegrationTest")]
-        public async Task Test_SignalSearch()
+        public async Task Test_SignalSearch_in_Tranas_municipality_when_having_access_to_Jonkoping_county()
         {
-            // To test with a specific user, change SOS.Lib.Managers.FilterManager.AddAuthorizationAsync() to use
-            // Remove: var user = await _userService.GetUserAsync();
-            // Add:    var user = await _userService.GetUserByIdAsync([userId]);
-
             //-----------------------------------------------------------------------------------------------------------
             // Arrange
             //-----------------------------------------------------------------------------------------------------------
+            var authorityBuilder = new UserAuthorizationTestBuilder();
+            var authority = authorityBuilder
+                .WithAuthorityIdentity("SightingIndication")
+                .WithMaxProtectionLevel(3)
+                .WithAreaAccess(TestData.AreaAuthority.JonkopingCounty)
+                .Build();
+            _fixture.UseMockUserService(authority);
+
             var searchFilter = new SignalFilterDto()
             {
                 Geographics = new GeographicsFilterDto()
@@ -77,5 +82,123 @@ namespace SOS.Observations.Api.IntegrationTests.IntegrationTests.ObservationsCon
             //-----------------------------------------------------------------------------------------------------------
             result.Should().BeTrue();
         }
+
+        [Fact]
+        [Trait("Category", "ApiIntegrationTest")]
+        public async Task Test_SignalSearch_in_Tranas_municipality_when_having_access_to_Ostergotland_county_and_consider_observation_accuracy()
+        {
+            //-----------------------------------------------------------------------------------------------------------
+            // Arrange
+            //-----------------------------------------------------------------------------------------------------------
+            var authorityBuilder = new UserAuthorizationTestBuilder();
+            var authority = authorityBuilder
+                .WithAuthorityIdentity("SightingIndication")
+                .WithMaxProtectionLevel(3)
+                .WithAreaAccess(TestData.AreaAuthority.OstergotlandCounty)
+                .Build();
+            _fixture.UseMockUserService(authority);
+
+            var searchFilter = new SignalFilterDto()
+            {
+                Geographics = new GeographicsFilterDto()
+                {
+                    Areas = new List<AreaFilterDto>
+                    {
+                        TestData.Areas.TranasMunicipality
+                    },
+                    ConsiderObservationAccuracy = true,
+                    ConsiderDisturbanceRadius = true,
+                    MaxAccuracy = 5000
+                },
+                Taxon = new TaxonFilterBaseDto()
+                {
+                    TaxonListIds = new[]
+                    {
+                        (int)TaxonListId.RedlistedSpecies,
+                        (int)TaxonListId.HabitatsDirective,
+                        (int)TaxonListId.ProtectedByLaw,
+                        (int)TaxonListId.ActionPlan,
+                        (int)TaxonListId.SwedishForestAgencyNatureConservationSpecies
+                    }
+                },
+                StartDate = new DateTime(1950, 1, 1)
+            };
+
+            //-----------------------------------------------------------------------------------------------------------
+            // Act
+            //-----------------------------------------------------------------------------------------------------------
+            var response = await _fixture.ObservationsController.SignalSearchInternal(
+                "CountyAdministrationObservation",
+                searchFilter,
+                false,
+                0,
+                true);
+            var result = response.GetResult<bool>();
+
+            //-----------------------------------------------------------------------------------------------------------
+            // Assert
+            //-----------------------------------------------------------------------------------------------------------
+            result.Should().BeTrue("because when consider observation accuracy some observations in Östergötland could intersect Tranås municipality in Jönköping county.");
+        }
+
+        [Fact]
+        [Trait("Category", "ApiIntegrationTest")]
+        public async Task Test_SignalSearch_in_Tranas_municipality_when_having_access_to_Ostergotland_county_and_not_consider_observation_accuracy()
+        {
+            //-----------------------------------------------------------------------------------------------------------
+            // Arrange
+            //-----------------------------------------------------------------------------------------------------------
+            var authorityBuilder = new UserAuthorizationTestBuilder();
+            var authority = authorityBuilder
+                .WithAuthorityIdentity("SightingIndication")
+                .WithMaxProtectionLevel(3)
+                .WithAreaAccess(TestData.AreaAuthority.OstergotlandCounty)
+                .Build();
+            _fixture.UseMockUserService(authority);
+
+            var searchFilter = new SignalFilterDto()
+            {
+                Geographics = new GeographicsFilterDto()
+                {
+                    Areas = new List<AreaFilterDto>
+                    {
+                        TestData.Areas.TranasMunicipality
+                    },
+                    ConsiderObservationAccuracy = false,
+                    ConsiderDisturbanceRadius = false,
+                    MaxAccuracy = 5000
+                },
+                Taxon = new TaxonFilterBaseDto()
+                {
+                    TaxonListIds = new[]
+                    {
+                        (int)TaxonListId.RedlistedSpecies,
+                        (int)TaxonListId.HabitatsDirective,
+                        (int)TaxonListId.ProtectedByLaw,
+                        (int)TaxonListId.ActionPlan,
+                        (int)TaxonListId.SwedishForestAgencyNatureConservationSpecies
+                    }
+                },
+                StartDate = new DateTime(1950, 1, 1)
+            };
+
+            //-----------------------------------------------------------------------------------------------------------
+            // Act
+            //-----------------------------------------------------------------------------------------------------------
+            var response = await _fixture.ObservationsController.SignalSearchInternal(
+                "CountyAdministrationObservation",
+                searchFilter,
+                false,
+                0,
+                true);
+            var result = response.GetResult<bool>();
+
+            //-----------------------------------------------------------------------------------------------------------
+            // Assert
+            //-----------------------------------------------------------------------------------------------------------
+            result.Should().BeFalse();
+        }
+
+
     }
 }
