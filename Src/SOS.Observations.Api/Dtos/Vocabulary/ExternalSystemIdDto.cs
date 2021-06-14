dd95e3b9f9086e743cac3b985d9ee6eb8f6f1e9f
Mats Lindgren <mats.lindgren@slu.se>
2021-06-14 15:15:38 +0200

    Application insights fix

--- Src/SOS.Observations.Api/Dtos/Vocabulary/ExternalSystemIdDto.cs
+++ Src/SOS.Observations.Api/Dtos/Vocabulary/ExternalSystemIdDto.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace SOS.Observations.Api.Dtos.Vocabulary
+﻿namespace SOS.Observations.Api.Dtos.Vocabulary
 {
     public enum ExternalSystemIdDto
     {
