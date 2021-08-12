dcf7eb7dba0e95ad52248f58e8d5f98db5e8cfa5
Mats Lindgren <mats.lindgren@slu.se>
2021-08-12 13:45:34 +0200

    Get locations by id

---
+++ Src/SOS.Observations.Api/Controllers/Interfaces/ILocationsController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SOS.Observations.Api.Controllers.Interfaces
+{
+    /// <summary>
+    ///     Observations controller interface.
+    /// </summary>
+    public interface ILocationsController
+    {
+        /// <summary>
+        /// Get locations by id
+        /// </summary>
+        /// <param name="locationIds"></param>
+        /// <returns></returns>
+        Task<IActionResult> GetLocationsAsync(IEnumerable<string> locationIds);
+    }
+}
