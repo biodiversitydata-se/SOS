69d3ad750297d3368a1c33d9fdc40c20270046ad
Martin Käck <martin.kack@slu.se>
2021-08-31 11:40:43 +0200

    Add support for culture code abbreviations

---
+++ Tests/Unit/SOS.Lib.UnitTests/Helpers/CultureCodeHelperTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using SOS.Lib.Helpers;
+using Xunit;
+
+namespace SOS.Lib.UnitTests.Helpers
+{
+    public class CultureCodeHelperTests
+    {
+        [Theory]
+        [InlineData("sv-SE", "sv-SE")]
+        [InlineData("sv-se", "sv-SE")]
+        [InlineData("sv", "sv-SE")]
+        [InlineData("SV", "sv-SE")]
+        [InlineData("swe", "sv-SE")]
+        [InlineData("", "sv-SE")]
+        [InlineData(null, "sv-SE")]
+        [InlineData("en-GB", "en-GB")]
+        [InlineData("en-gb", "en-GB")]
+        [InlineData("en", "en-GB")]
+        [InlineData("EN", "en-GB")]
+        [InlineData("eng", "en-GB")]
+        public void Resolve_cultureCode(
+            string cultureCode,
+            string expected)
+        {
+            //-----------------------------------------------------------------------------------------------------------
+            // Act
+            //-----------------------------------------------------------------------------------------------------------
+            var result = CultureCodeHelper.GetCultureCode(cultureCode);
+
+            //-----------------------------------------------------------------------------------------------------------
+            // Assert
+            //-----------------------------------------------------------------------------------------------------------
+            result.Should().Be(expected);
+        }
+    }
+}
