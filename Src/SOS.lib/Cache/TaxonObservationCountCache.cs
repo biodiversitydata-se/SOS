7ab8c54a445070ab525a4f966263eb945bbefcf5
Martin Käck <martin.kack@slu.se>
2021-10-18 14:57:16 +0200

    Improve TaxonObservationCountCache memory utilization.

--- Src/SOS.lib/Cache/TaxonObservationCountCache.cs
+++ Src/SOS.lib/Cache/TaxonObservationCountCache.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using Microsoft.Extensions.Caching.Memory;
 using SOS.Lib.Cache.Interfaces;
 using SOS.Lib.Models.Cache;
@@ -12,33 +13,53 @@ namespace SOS.Lib.Cache
     {
         private readonly MemoryCache _memoryCache = new MemoryCache(new MemoryCacheOptions());
         private readonly MemoryCacheEntryOptions _cacheEntryOptions = new MemoryCacheEntryOptions()
-            .SetAbsoluteExpiration(TimeSpan.FromHours(4))
-            .SetSize(1);
+            .SetAbsoluteExpiration(TimeSpan.FromHours(4));
         private long _entriesCounter;
-        private const int NumberOfEntriesCleanupLimit = 100000; // The cache will grow to approximately 200MB with limit=100000.
+        private const int NumberOfEntriesCleanupLimit = 2000000; // The cache will grow to approximately 250MB with limit=2000000.
+        private const string CacheKey = "TaxonObservationCountCache";
+        private readonly object _lockObject = new object();
+
+        private ConcurrentDictionary<TaxonObservationCountCacheKey, int> Cache
+        {
+            get
+            {
+                ConcurrentDictionary<TaxonObservationCountCacheKey, int> dictionary;
+                if (_memoryCache.TryGetValue(CacheKey, out dictionary))
+                {
+                    return dictionary;
+                }
+
+                dictionary = new ConcurrentDictionary<TaxonObservationCountCacheKey, int>();
+                _memoryCache.Set(CacheKey, dictionary, _cacheEntryOptions);
+                return dictionary;
+            }
+        }
 
         public void Add(TaxonObservationCountCacheKey taxonObservationCountCacheKey, int count)
         {
-            try
+            lock(_lockObject)
             {
-                _memoryCache.Set(taxonObservationCountCacheKey, count, _cacheEntryOptions);
                 _entriesCounter++;
                 if (_entriesCounter > NumberOfEntriesCleanupLimit)
                 {
                     _entriesCounter = 0;
-                    _memoryCache.Compact(0.5);
+                    CleanUp();
                 }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-                throw;
+                Cache.AddOrUpdate(taxonObservationCountCacheKey, count, (key, oldValue) => count);
             }
         }
 
         public bool TryGetCount(TaxonObservationCountCacheKey taxonObservationCountCacheKey, out int count)
         {
-            return _memoryCache.TryGetValue(taxonObservationCountCacheKey, out count);
+            lock (_lockObject)
+            {
+                return Cache.TryGetValue(taxonObservationCountCacheKey, out count);
+            }
+        }
+
+        private void CleanUp()
+        {
+            Cache.Clear();
         }
     }
 }
