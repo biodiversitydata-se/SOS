f01d07403d4d807295bf27331408a8656f089411
Martin Käck <martin.kack@slu.se>
2021-11-18 22:03:01 +0100

    Store export job info enums as string in MongoDb

--- Src/SOS.lib/Models/Export/ExportJobInfo.cs
+++ Src/SOS.lib/Models/Export/ExportJobInfo.cs
@@ -1,4 +1,6 @@
-﻿using SOS.Lib.Enums;
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using SOS.Lib.Enums;
 using SOS.Lib.Models.Interfaces;
 using System;
 using System.Collections.Generic;
@@ -17,9 +19,12 @@ namespace SOS.Lib.Models.Export
         public TimeSpan? ProcessingTime { get; set; }
         public int? NumberOfObservations { get; set; }
         public string Description { get; set; }
+        [BsonRepresentation(BsonType.String)]
         public ExportFormat Format { get; set; }
+        [BsonRepresentation(BsonType.String)]
         public ExportJobStatus Status { get; set; }
         public string ErrorMsg { get; set; }
+        [BsonRepresentation(BsonType.String)]
         public OutputFieldSet? OutputFieldSet { get; set; }
     }
 }
