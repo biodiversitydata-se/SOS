233e40f4c22d8f8ef7085a4bca76a6398afa06c9
Martin Käck <martin.kack@slu.se>
2021-11-22 16:38:09 +0100

    Add MongoDb collection copy tool

---
+++ Tests/Integration/SOS.Observations.Api.IntegrationTests/TestDataTools/CopyMongoDbCollectionTool.cs
@@ -0,0 +1,57 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using SOS.Lib.Configuration.Shared;
+using SOS.Lib.Database;
+using SOS.Lib.Database.Interfaces;
+using SOS.Lib.Models.Verbatim.ClamPortal;
+using SOS.Observations.Api.IntegrationTests.Fixtures;
+using SOS.Observations.Api.IntegrationTests.Utils;
+using SOS.TestHelpers;
+using Xunit;
+
+namespace SOS.Observations.Api.IntegrationTests.TestDataTools
+{
+    public class CopyMongoDbCollectionTool : FixtureBase
+    {        
+        [Fact]
+        [Trait("Category", "ApiIntegrationTest")]
+        public async Task CopyClamPortalObservations()
+        {
+            //-----------------------------------------------------------------------------------------------------------
+            // Arrange
+            //-----------------------------------------------------------------------------------------------------------
+            var fromClient = GetProcessClient(InstallationEnvironment.SystemTest, "sos-harvest-st");
+            var toClient = GetProcessClient(InstallationEnvironment.Production, "sos-harvest");            
+            var mongoDbUtil = new MongoDbUtil(fromClient, toClient);            
+
+            //-----------------------------------------------------------------------------------------------------------
+            // Act
+            //-----------------------------------------------------------------------------------------------------------
+            await mongoDbUtil.CopyCollectionAsync<ClamObservationVerbatim>("ClamObservationVerbatim", "ClamObservationVerbatim_copy");
+
+            //-----------------------------------------------------------------------------------------------------------
+            // Assert
+            //-----------------------------------------------------------------------------------------------------------                        
+        }
+
+
+        private IProcessClient GetProcessClient(InstallationEnvironment environment, string databaseName = null)
+        {
+            var mongoDbConfiguration = GetMongoDbConfiguration(environment);
+            // Change database name if specified. Otherwise use the one specified in appsettings.
+            if (databaseName != null) mongoDbConfiguration.DatabaseName = databaseName;
+            var processedSettings = mongoDbConfiguration.GetMongoDbSettings();
+            var processClient = new ProcessClient(processedSettings, mongoDbConfiguration.DatabaseName,
+                mongoDbConfiguration.ReadBatchSize, mongoDbConfiguration.WriteBatchSize);
+            return processClient;
+        }        
+
+        protected MongoDbConfiguration GetMongoDbConfiguration(InstallationEnvironment environment)
+        {
+            var config = GetAppSettings();
+            var configPrefix = GetConfigPrefix(environment);
+            var mongoDbConfiguration = config.GetSection($"{configPrefix}:ProcessDbConfiguration").Get<MongoDbConfiguration>();
+            return mongoDbConfiguration;
+        }
+    }
+}
