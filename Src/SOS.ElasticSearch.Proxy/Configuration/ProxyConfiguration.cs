854a3e96cabe397be0ef27ed2c694451a39d9a80
Martin Käck <martin.kack@slu.se>
2022-04-21 10:45:05 +0200

    Add ExcludeFields settings to SOS.ElasticSearch.Proxy

--- Src/SOS.ElasticSearch.Proxy/Configuration/ProxyConfiguration.cs
+++ Src/SOS.ElasticSearch.Proxy/Configuration/ProxyConfiguration.cs
@@ -31,5 +31,35 @@
         /// If True, exclude fields should be added to the JSON query sent to Elasticsearch.
         /// </summary>
         public bool ExcludeFieldsInElasticsearchQuery { get; set; }
+
+        /// <summary>
+        /// Fields to exclude in Elasticsearch query.
+        /// </summary>
+        public List<string>? ExcludeFields { get; set; }
+
+        /// <summary>
+        /// The exclude query.
+        /// </summary>
+        public string? ExcludeFieldsQuery
+        {
+            get
+            {
+                if (_excludeFieldsQuery == null)
+                {
+                    if (ExcludeFields == null || ExcludeFields.Count == 0)
+                    {
+                        _excludeFieldsQuery = "";
+                    }
+                    else
+                    {
+                        string fields = string.Join(", ", ExcludeFields.Select(m => $"\"{m}\""));
+                        _excludeFieldsQuery = $"\"_source\": {{ \"excludes\": [{fields}]}},";
+                    }
+                }
+
+                return _excludeFieldsQuery;
+            }
+        }
+        private string? _excludeFieldsQuery;
     }
 }
