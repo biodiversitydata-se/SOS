5b427c7426432903ce479693ec4bdec43f654878
Martin Käck <martin.kack@slu.se>
2022-04-26 18:56:58 +0200

    Add ObjectHelper unit test.

---
+++ Tests/Unit/SOS.Lib.UnitTests/Helpers/ObjectHelperTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using SOS.Lib.Helpers;
+using SOS.Lib.Models.Processed.Observation;
+using Xunit;
+
+namespace SOS.Lib.UnitTests.Helpers
+{
+    public class ObjectHelperTests
+    {
+        [Fact]
+        public void TestIdentifyGarbageChars()
+        {
+            //-----------------------------------------------------------------------------------------------------------
+            // Arrange
+            //-----------------------------------------------------------------------------------------------------------
+            var objectHelper = new ObjectHelper();
+            var observation = new Observation
+            {
+                Projects = new List<Project>
+                {
+                    new Project
+                    {
+                        ProjectParameters = new List<ProjectParameter>
+                        {
+                            new ProjectParameter
+                            {
+                                Value = "test\twith tab"
+                            }
+                        }
+                    }
+                }
+            };
+
+            //-----------------------------------------------------------------------------------------------------------
+            // Act
+            //-----------------------------------------------------------------------------------------------------------
+            var result = objectHelper.GetPropertiesWithGarbageChars(observation);
+
+            //-----------------------------------------------------------------------------------------------------------
+            // Assert
+            //-----------------------------------------------------------------------------------------------------------
+            result.First().Should().Be("Projects.ProjectParameters.Value", "because that property contains a tab character");
+        }
+    }
+}
