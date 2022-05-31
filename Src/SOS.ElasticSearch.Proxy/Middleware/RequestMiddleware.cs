042a6efcc4057c4204288d677fc221a356612eec
Martin Käck <martin.kack@slu.se>
2022-05-31 09:34:23 +0200

    WFS sort fix.

--- Src/SOS.ElasticSearch.Proxy/Middleware/RequestMiddleware.cs
+++ Src/SOS.ElasticSearch.Proxy/Middleware/RequestMiddleware.cs
@@ -140,10 +140,10 @@ namespace SOS.ElasticSearch.Proxy.Middleware
                 {
                     _logger.LogInformation($"Query before sort change: {query}");
                 }
-                query = query.Replace("\"sort\":[{ \"_id\":{ \"order\":\"asc\"} }",
-                    "\"sort\":[{\"event.endDate\":{ \"order\":\"desc\"} }");
-                query = query.Replace("\"sort\":[{ \"_id\":{ \"order\":\"desc\"} }",
-                    "\"sort\":[{\"event.endDate\":{ \"order\":\"desc\"} }");
+                query = query.Replace("\"sort\":[{\"_id\":{\"order\":\"asc\"}}",
+                    "\"sort\":[{\"event.endDate\":{\"order\":\"desc\"}}");
+                query = query.Replace("\"sort\":[{\"_id\":{\"order\":\"desc\"}}",
+                    "\"sort\":[{\"event.endDate\":{\"order\":\"desc\"}}");
                 if (_proxyConfiguration.LogRequest)
                 {
                     _logger.LogInformation($"Query after sort change: {query}");
