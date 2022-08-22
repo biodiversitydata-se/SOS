b25d878c814566dbd0b848154558ad4d92a4ea5b
Mats Lindgren <mats.lindgren@slu.se>
2022-08-22 09:42:31 +0200

    Merge

--- Tests/Integration/SOS.AutomaticIntegrationTests/TestFixtures/IntegrationTestFixture.cs
+++ Tests/Integration/SOS.AutomaticIntegrationTests/TestFixtures/IntegrationTestFixture.cs
@@ -314,11 +314,9 @@ namespace SOS.AutomaticIntegrationTests.TestFixtures
             var exportManager = new ExportManager(csvFileWriter, dwcArchiveFileWriter, excelFileWriter, geojsonFileWriter,
                 processedObservationRepository, processInfoRepository, filterManager, new NullLogger<ExportManager>());
             var userExportRepository = new UserExportRepository(_processClient, new NullLogger<UserExportRepository>());
-            var userStatisticsManager = new UserStatisticsManager(processedObservationRepository, filterManager,
-                new NullLogger<UserStatisticsManager>());
-            UserStatisticsController = new UserStatisticsController(userStatisticsManager, areaManager, new ObservationApiConfiguration(),
+          
             var userStatisticsManager = new UserStatisticsManager(UserObservationRepository, processedObservationRepository, new NullLogger<UserStatisticsManager>());
-            UserStatisticsController = new UserStatisticsController(userStatisticsManager, taxonManager, areaManager,
+            UserStatisticsController = new UserStatisticsController(userStatisticsManager, areaManager, new ObservationApiConfiguration(),
                 new NullLogger<UserStatisticsController>());
             ObservationsController = new ObservationsController(observationManager, taxonSearchManager, taxonManager, areaManager, observationApiConfiguration, elasticConfiguration, new NullLogger<ObservationsController>());
             var checklistManager = new ChecklistManager(ProcessedChecklistRepository, processedObservationRepository, filterManager, new NullLogger<ChecklistManager>());
