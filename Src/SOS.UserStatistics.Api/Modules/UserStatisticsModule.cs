4bd8639eca6ff9bfbbe7b20f287b0889c21efcc8
Mattias Andersson <mattias.andersson@slu.se>
2022-09-19 14:35:56 +0200

    Tidying

--- Src/SOS.UserStatistics.Api/Modules/UserStatisticsModule.cs
+++ Src/SOS.UserStatistics.Api/Modules/UserStatisticsModule.cs
@@ -14,6 +14,9 @@ public class UserStatisticsModule : Interfaces.IModule
         .Produces(StatusCodes.Status401Unauthorized)
         .Produces(StatusCodes.Status500InternalServerError);
 
+        /// <summary>
+        /// Aggregates taxon by user.
+        /// </summary>
         application.MapPost("userstatistics/pagedspeciescountaggregation", async (IUserStatisticsManager userStatisticsManager,
             [FromBody] SpeciesCountUserStatisticsQuery query, 
             [FromQuery] int? skip, int? take, bool useCache) =>
@@ -27,6 +30,9 @@ public class UserStatisticsModule : Interfaces.IModule
         .Produces(StatusCodes.Status401Unauthorized)
         .Produces(StatusCodes.Status500InternalServerError);
 
+        /// <summary>
+        /// Aggregates taxon by user.
+        /// </summary>
         application.MapPost("userstatistics/speciescountaggregation", async (IUserStatisticsManager userStatisticsManager,
             [FromBody] SpeciesCountUserStatisticsQuery query, 
             [FromQuery] int? skip, 
@@ -42,6 +48,10 @@ public class UserStatisticsModule : Interfaces.IModule
         .Produces(StatusCodes.Status401Unauthorized)
         .Produces(StatusCodes.Status500InternalServerError);
 
+        /// <summary>
+        /// Experimental. The functionality should be the same as PagedSpeciesCountAggregation but without the need to create a new index.
+        /// Aggregates taxon by user. 
+        /// </summary>
         application.MapPost("userstatistics/processedobservationpagedspeciescountaggregation", async (IUserStatisticsManager userStatisticsManager,
             [FromBody] SpeciesCountUserStatisticsQuery query, 
             [FromQuery] int? skip, 
