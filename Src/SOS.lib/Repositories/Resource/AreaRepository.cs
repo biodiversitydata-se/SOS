3db29ad425c35aeecd55daf57c8c4ff583944c6f
Mats Lindgren <mats.lindgren@slu.se>
2023-02-22 12:23:25 +0100

    Economic zone of sweden search bug

--- Src/SOS.lib/Repositories/Resource/AreaRepository.cs
+++ Src/SOS.lib/Repositories/Resource/AreaRepository.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Text.Json;
@@ -114,6 +115,11 @@ namespace SOS.Lib.Repositories.Resource
             if (areaTypes?.Any() ?? false)
             {
                 filters.Add(Builders<Area>.Filter.In(a => a.AreaType, areaTypes));
+            }
+            else
+            {
+                // Make sure economic zone of sweden is NOT matched
+                filters.Add(Builders<Area>.Filter.In(a => a.AreaType, ((AreaType[])Enum.GetValues(typeof(AreaType))).Where(at => at != AreaType.EconomicZoneOfSweden))); 
             }
 
             if (!string.IsNullOrEmpty(searchString))
