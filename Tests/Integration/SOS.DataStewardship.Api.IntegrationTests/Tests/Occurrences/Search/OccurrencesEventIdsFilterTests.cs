2377222618e94beb1ffd785bbace0b323b98c3df
Martin Käck <martin.kack@slu.se>
2023-03-02 14:00:57 +0100

    Add data stewardship occurrences datasetIds & eventIds tests.

---
+++ Tests/Integration/SOS.DataStewardship.Api.IntegrationTests/Tests/Occurrences/Search/OccurrencesEventIdsFilterTests.cs
@@ -0,0 +1,28 @@
+namespace SOS.DataStewardship.Api.IntegrationTests.Tests.Occurrences.Search;
+
+[Collection(Constants.IntegrationTestsCollectionName)]
+public class OccurrencesEventIdsFilterTests : TestBase
+{
+    public OccurrencesEventIdsFilterTests(TestFixture testFixture, ITestOutputHelper output) : base(testFixture, output)
+    {
+    }
+
+    [Fact]
+    public async Task OccurrencesBySearch_ReturnsExpectedOccurrences_GivenExistingEventId()
+    {
+        // Arrange
+        var testDataSet = TestData.GetTestDataSet(10);
+        string eventId = testDataSet.Observations.First().Event.EventId = Guid.NewGuid().ToString();
+        await ProcessFixture.AddObservationsToElasticsearchAsync(testDataSet.Observations);
+        var searchFilter = new OccurrenceFilter() {
+            EventIds = new List<string>() { eventId }            
+        };
+        
+        // Act
+        var pageResult = await ApiClient.PostAndReturnAsJsonAsync<PagedResult<OccurrenceModel>, OccurrenceFilter>(
+            $"datastewardship/occurrences?skip=0&take=0", searchFilter, jsonSerializerOptions);
+
+        // Assert
+        pageResult.TotalCount.Should().Be(1);
+    }
+}
