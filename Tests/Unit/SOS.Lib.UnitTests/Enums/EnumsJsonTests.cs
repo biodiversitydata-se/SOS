0be7c4a6aa66289e6ba61bd8b813149ec14a5334
Martin Käck <martin.kack@slu.se>
2023-05-04 15:40:29 +0200

    Add JSON enum parsing tests

---
+++ Tests/Unit/SOS.Lib.UnitTests/Enums/EnumsJsonTests.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using FluentAssertions;
+using SOS.Lib.Enums;
+using Xunit;
+
+namespace SOS.Lib.UnitTests.Models.TaxonTree
+{
+    public class EnumsJsonTests
+    {       
+        string jsonWithEnumValue = """
+        {
+            "LocationType": 2 
+        }
+        """;
+
+        string jsonWithEnumStringValue = """
+        {
+            "LocationType": "Polygon"
+        }
+        """;
+
+        private class MyClass
+        {
+            public LocationType LocationType { get; set; }
+        }
+
+        [Fact]
+        public void ParseJson_Succeeds_GivenJsonWithEnumValue()
+        {            
+            //-----------------------------------------------------------------------------------------------------------
+            // Act
+            //-----------------------------------------------------------------------------------------------------------
+            var obj = JsonSerializer.Deserialize<MyClass>(jsonWithEnumValue);
+
+            //-----------------------------------------------------------------------------------------------------------
+            // Assert
+            //-----------------------------------------------------------------------------------------------------------
+            obj.Should().NotBeNull();
+        }
+
+        [Fact]
+        public void ParseJson_Fails_GivenJsonWithEnumString()
+        {
+            //-----------------------------------------------------------------------------------------------------------
+            // Act
+            //-----------------------------------------------------------------------------------------------------------
+            Action act = () => JsonSerializer.Deserialize<MyClass>(jsonWithEnumStringValue);
+
+            //-----------------------------------------------------------------------------------------------------------
+            // Assert
+            //-----------------------------------------------------------------------------------------------------------
+            act.Should().Throw<JsonException>();                
+        }
+
+        [Fact]
+        public void ParseJson_Succeeds_GivenJsonWithEnumStringAndEnumConverter()
+        {
+            //-----------------------------------------------------------------------------------------------------------
+            // Arrange
+            //-----------------------------------------------------------------------------------------------------------
+            var jsonOptions = new JsonSerializerOptions
+            {
+                Converters = { new JsonStringEnumConverter() }
+            };
+
+            //-----------------------------------------------------------------------------------------------------------
+            // Act
+            //-----------------------------------------------------------------------------------------------------------
+            var obj = JsonSerializer.Deserialize<MyClass>(jsonWithEnumStringValue, jsonOptions);
+
+            //-----------------------------------------------------------------------------------------------------------
+            // Assert
+            //-----------------------------------------------------------------------------------------------------------
+            obj.Should().NotBeNull();
+        }
+    }
+}
