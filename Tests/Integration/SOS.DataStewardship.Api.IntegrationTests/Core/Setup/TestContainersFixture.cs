161855059952de44234801a7222e388a5e359cff
Martin Käck <martin.kack@slu.se>
2023-05-05 11:35:09 +0200

    Update TestContainersFixture

--- Tests/Integration/SOS.DataStewardship.Api.IntegrationTests/Core/Setup/TestContainersFixture.cs
+++ Tests/Integration/SOS.DataStewardship.Api.IntegrationTests/Core/Setup/TestContainersFixture.cs
@@ -92,7 +92,7 @@ namespace SOS.DataStewardship.Api.IntegrationTests.Core.Setup
 
         private async Task RestoreMongoDbBackup(MongoDbContainer mongoDbTestcontainer)
         {
-            string filePath = @"data\resources\mongodb-sos-dev.gz".GetAbsoluteFilePath();
+            string filePath = @"data/resources/mongodb-sos-dev.gz".GetAbsoluteFilePath();
             byte[] mongoDbBackupBytes = await File.ReadAllBytesAsync(filePath);
             await mongoDbTestcontainer.CopyFileAsync("/dump/mongodb-sos-dev.gz", mongoDbBackupBytes);
             var cmds = new List<string>()
