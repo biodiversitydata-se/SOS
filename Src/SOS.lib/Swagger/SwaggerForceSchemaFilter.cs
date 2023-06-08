73d6d890f9558f434256ef7cb777f2f362f67253
Mats Lindgren <mats.lindgren@slu.se>
2023-06-08 09:11:01 +0200

    Changed OpenApi schema. Implemented attribute to manually set datatype for a property in schema

---
+++ Src/SOS.lib/Swagger/SwaggerForceSchemaFilter.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.OpenApi.Models;
+using SOS.Lib.Extensions;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System.Linq;
+using System.Reflection;
+
+namespace SOS.Lib.Swagger
+{
+    /// <summary>
+    ///     Swagger ignore filter
+    /// </summary>
+    public class SwaggerForceSchemaFilter : ISchemaFilter
+    {
+        /// <summary>
+        ///     Apply filter
+        /// </summary>
+        /// <param name="schema"></param>
+        /// <param name="context"></param>
+        public void Apply(OpenApiSchema schema, SchemaFilterContext context)
+        {
+            var propertyInfos = context.Type?.GetProperties(BindingFlags.Public | BindingFlags.Instance)
+               .Where(p => p.GetCustomAttribute<UseSchema>() != null);
+
+            if (propertyInfos != null)
+            {
+                foreach(var propertyInfo in propertyInfos)
+                {
+                    var propertyName = propertyInfo.Name.ToCamelCase();
+                    if (schema.Properties.ContainsKey(propertyName))
+                    {
+                        schema.Properties[propertyName] = propertyInfo?.GetCustomAttribute<UseSchema>()?.Schema;
+                    }
+                }
+            }
+        }
+    }
+}
