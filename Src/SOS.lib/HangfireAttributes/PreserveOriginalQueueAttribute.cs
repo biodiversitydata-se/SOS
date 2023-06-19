31fdb51fd7eadcbbb1868ba0e6e44387db4a2d5a
Mats Lindgren <mats.lindgren@slu.se>
2023-06-19 10:31:58 +0200

    Export Queue fix

---
+++ Src/SOS.lib/HangfireAttributes/PreserveOriginalQueueAttribute.cs
@@ -0,0 +1,52 @@
+﻿using Hangfire.Common;
+using Hangfire.States;
+using Hangfire.Storage;
+
+namespace SOS.Lib.HangfireAttributes
+{
+    public class PreserveOriginalQueue : JobFilterAttribute, IApplyStateFilter
+    {
+        public void OnStateApplied(ApplyStateContext context, IWriteOnlyTransaction transaction)
+        {
+            try
+            {
+                // Activating only when enqueueing a background job
+                if (context.NewState is not EnqueuedState enqueuedState) {
+                    return;
+                };
+
+                // Checking if an original queue is already set
+                var originalQueue = SerializationHelper.Deserialize<string>(
+                    context.Connection.GetJobParameter(
+                        context.BackgroundJob.Id,
+                        "OriginalQueue")
+                );
+
+                var x = context.Connection.GetJobParameter(
+                        context.BackgroundJob.Id,
+                        "OriginalQueue");
+
+                if (originalQueue != null)
+                {
+                    // Override any other queue value that is currently set (by other filters, for example)
+                    enqueuedState.Queue = originalQueue;
+                }
+                else
+                {
+                    // Queueing for the first time, we should set the original queue
+                    context.Connection.SetJobParameter(
+                        context.BackgroundJob.Id,
+                        "OriginalQueue",
+                        SerializationHelper.Serialize(enqueuedState.Queue));
+                }
+            }
+            catch { }
+        }
+
+
+        public void OnStateUnapplied(ApplyStateContext context, IWriteOnlyTransaction transaction)
+        {
+            // Required to match IApplyStateFilter
+        }
+    }
+}
