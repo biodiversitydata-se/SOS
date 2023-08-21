4dfa7edcf26cbfc07a330a44432dd8a81adb192b
Martin Käck <martin.kack@slu.se>
2023-08-21 11:44:09 +0200

    Add Area export integration tests

---
+++ Tests/Integration/SOS.ContainerIntegrationTests/TestData/AreaFeatureIds.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOS.ContainerIntegrationTests.TestData;
+internal static class AreaFeatureIds
+{
+    public static string Enköping = "381";
+}
