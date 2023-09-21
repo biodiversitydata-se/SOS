472cc2924471b70582bc46883396eeee88c6f4e3
Martin Käck <martin.kack@slu.se>
2023-09-21 14:17:19 +0200

    Add possibility to start Kibana container when running integration tests

--- Tests/Integration/SOS.Observations.Api.IntegrationTests/Setup/ContainerDbFixtures/TestContainersFixture.cs
+++ Tests/Integration/SOS.Observations.Api.IntegrationTests/Setup/ContainerDbFixtures/TestContainersFixture.cs
@@ -7,10 +7,12 @@ using SOS.Lib.Database;
 using Testcontainers.Elasticsearch;
 using Testcontainers.MongoDb;
 using SOS.Observations.Api.IntegrationTests.Extensions;
+using DotNet.Testcontainers.Builders;
 
 namespace SOS.Observations.Api.IntegrationTests.Setup.ContainerDbFixtures;
 public class TestContainersFixture : IAsyncLifetime
 {
+    private bool UseKibanaDebug;
     private const string ELASTIC_PASSWORD = "elastic";
     private const string ELASTIC_IMAGE_NAME = "elasticsearch:8.7.1";
 
@@ -30,18 +32,7 @@ public class TestContainersFixture : IAsyncLifetime
 
     public TestContainersFixture()
     {
-        ElasticsearchContainer = new ElasticsearchBuilder()
-            .WithImage(ELASTIC_IMAGE_NAME)
-            .WithCleanUp(true)
-            .WithPassword(ELASTIC_PASSWORD)
-            .Build();
-
-        MongoDbContainer = new MongoDbBuilder()
-            .WithImage(MONGODB_IMAGE_NAME)
-            .WithCleanUp(true)
-            .WithUsername(MONGODB_USERNAME)
-            .WithPassword(MONGODB_PASSWORD)
-            .Build();
+        UseKibanaDebug = false;
     }
 
     public async Task InitializeAsync()
@@ -71,9 +62,55 @@ public class TestContainersFixture : IAsyncLifetime
     }
 
     private async Task<ElasticClient> InitializeElasticsearchAsync()
-    {
-        await ElasticsearchContainer.StartAsync().ConfigureAwait(false);
+    {   
+        if (UseKibanaDebug)
+        {
+            return await InitializeElasticsearchWithKibanaAsync();
+        }
+
+        ElasticsearchContainer = new ElasticsearchBuilder()
+            .WithImage(ELASTIC_IMAGE_NAME)
+            .WithCleanUp(true)
+            .WithPassword(ELASTIC_PASSWORD)                        
+        .Build();
+        await ElasticsearchContainer.StartAsync().ConfigureAwait(false);               
         var elasticClient = new ElasticClient(new ConnectionSettings(new Uri(ElasticsearchContainer.GetConnectionString()))
+            .ServerCertificateValidationCallback(CertificateValidations.AllowAll)
+            .EnableApiVersioningHeader()
+        .EnableDebugMode());
+
+        return elasticClient;
+    }
+
+    private async Task<ElasticClient> InitializeElasticsearchWithKibanaAsync()
+    {
+        var network = new NetworkBuilder()
+            .WithName(Guid.NewGuid().ToString("D"))
+        .Build();
+
+        ElasticsearchContainer = new ElasticsearchBuilder()
+            .WithImage(ELASTIC_IMAGE_NAME)
+            .WithCleanUp(true)            
+            .WithPortBinding(9200, 9200)
+            .WithNetwork(network)
+            .WithNetworkAliases("elastic-test-network")            
+            .WithEnvironment("xpack.security.enabled", "false")        
+        .Build();
+
+        var kibanaContainer = new ContainerBuilder()
+          .WithName(Guid.NewGuid().ToString("D"))
+          .WithImage("docker.elastic.co/kibana/kibana:8.7.1")
+          .WithPortBinding(5601, 5601)
+          .WithNetwork(network)
+          .WithEnvironment("ELASTICSEARCH_HOSTS", $"http://elastic-test-network:9200")        
+        .Build();
+
+        await network.CreateAsync().ConfigureAwait(false);
+        await ElasticsearchContainer.StartAsync().ConfigureAwait(false);
+        await kibanaContainer.StartAsync().ConfigureAwait(false);
+        
+        var elasticUri = new UriBuilder(Uri.UriSchemeHttp, ElasticsearchContainer.Hostname, ElasticsearchContainer.GetMappedPublicPort(9200)).ToString();
+        var elasticClient = new ElasticClient(new ConnectionSettings(new Uri(elasticUri))
             .ServerCertificateValidationCallback(CertificateValidations.AllowAll)
             .EnableApiVersioningHeader()
         .EnableDebugMode());
@@ -82,6 +119,13 @@ public class TestContainersFixture : IAsyncLifetime
 
     private async Task<ProcessClient> InitializeMongoDbAsync()
     {
+        MongoDbContainer = new MongoDbBuilder()
+            .WithImage(MONGODB_IMAGE_NAME)
+            .WithCleanUp(true)
+            .WithUsername(MONGODB_USERNAME)
+            .WithPassword(MONGODB_PASSWORD)
+            .Build();
+
         await MongoDbContainer.StartAsync().ConfigureAwait(false);
         await RestoreMongoDbBackup(MongoDbContainer);
         var mongoClientSettings = MongoClientSettings.FromConnectionString(MongoDbContainer.GetConnectionString());
