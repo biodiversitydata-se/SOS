2af0db30c39951011744af3752bca97ec5914a69
Martin Käck <martin.kack@slu.se>
2023-10-06 15:35:09 +0200

    Add compare taxon trees test

--- Tests/Unit/SOS.Import.UnitTests/Services/Taxon/TaxonServiceTests.cs
+++ Tests/Unit/SOS.Import.UnitTests/Services/Taxon/TaxonServiceTests.cs
@@ -5,6 +5,7 @@ using SOS.Harvest.Services.Taxon;
 using SOS.Harvest.Services.Taxon.Interfaces;
 using SOS.Lib.Extensions;
 using SOS.Lib.Factories;
+using System.Linq;
 using Xunit;
 
 namespace SOS.Import.UnitTests.Services.Taxon
@@ -32,6 +33,51 @@ namespace SOS.Import.UnitTests.Services.Taxon
             // Assert
             //-----------------------------------------------------------------------------------------------------------
             taxonTree.Should().NotBeNull();
+        }
+
+        [Fact(Skip = "Intended to run on demand")]
+        public void CompareDyntaxaDwcaFiles()
+        {
+            // -----------------------------------------------------------------------------------------------------------
+            // Arrange
+            //-----------------------------------------------------------------------------------------------------------
+            var taxonService = new TaxonService(new Mock<ITaxonServiceProxy>().Object,
+                new Lib.Configuration.Process.TaxonServiceConfiguration() { BaseAddress = "" },
+                new NullLogger<TaxonService>());
+
+            //-----------------------------------------------------------------------------------------------------------
+            // Act
+            //-----------------------------------------------------------------------------------------------------------
+            var dwcTaxa1 = taxonService.GetTaxaFromDwcaFile(@"C:\Temp\2023-10-04\dyntaxa-custom-dwca.zip2023-09-30T05_44_22.zip");
+            var taxa1 = dwcTaxa1.ToProcessedTaxa();
+            var taxonTree1 = TaxonTreeFactory.CreateTaxonTree(taxa1);
+
+            var dwcTaxa2 = taxonService.GetTaxaFromDwcaFile(@"C:\Temp\2023-10-04\dyntaxa-custom-dwca.zip2023-10-04T05_45_54.zip");
+            System.Collections.Generic.IDictionary<int, Lib.Models.Processed.Observation.Taxon> taxa2 = dwcTaxa2.ToProcessedTaxa();
+            var taxonTree2 = TaxonTreeFactory.CreateTaxonTree(taxa2);
+
+            var additionalTaxaInWorkingFile = taxa2.Values.ExceptBy(taxa1.Values.Select(m => m.Id), m => m.Id);
+            var summary = additionalTaxaInWorkingFile.Select(m => new TaxonSummary(m));
+
+            //-----------------------------------------------------------------------------------------------------------
+            // Assert
+            //-----------------------------------------------------------------------------------------------------------
+            taxonTree1.Should().NotBeNull();
+        }
+
+        public class TaxonSummary
+        {
+            public Lib.Models.Processed.Observation.Taxon Taxon { get; set; }
+
+            public TaxonSummary(Lib.Models.Processed.Observation.Taxon taxon)
+            {
+                Taxon = taxon;
+            }
+
+            public override string ToString()
+            {
+                return $"Category={Taxon.TaxonRank}, Name={Taxon.VernacularName} ";
+            }
         }
     }
 }
