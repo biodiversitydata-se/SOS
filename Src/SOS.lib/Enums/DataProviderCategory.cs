027f51d068e184d9109cae543f15f65620df2c06
Martin Käck <martin.kack@slu.se>
2024-03-15 10:35:54 +0100

    Update DefaultDataProviders.json

--- Src/SOS.lib/Enums/DataProviderCategory.cs
+++ Src/SOS.lib/Enums/DataProviderCategory.cs
@@ -4,24 +4,22 @@ namespace SOS.Lib.Enums
     public enum DataProviderCategory
     {
         DataHostesship,
-
         NationalInventory,
         RegionalInventory,
         MuseumCollections,
         HerbariaCollections,
         CitizenSciencePlatform,
-
         Atlas,
         Terrestrial,
         Freshwater,
         Marine,
-
         Vertebrates,
         Invertebrates,
         Arthropods,
         Microorganisms,
         Plants_Bryophytes_Lichens,
         Fungi,
-        Algae
+        Algae,
+        DataStewardship
     }
 }
