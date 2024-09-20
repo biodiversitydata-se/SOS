7162b2ec3f8f81fa41412fd93866fc20fa0dfc87
Martin Käck <martin.kack@slu.se>
2024-09-20 13:43:55 +0200

    Add feature flag support for UserAdmin2 User API

--- Src/SOS.lib/Configuration/Shared/UserServiceConfiguration.cs
+++ Src/SOS.lib/Configuration/Shared/UserServiceConfiguration.cs
@@ -2,5 +2,14 @@
 {
     public class UserServiceConfiguration : RestServiceConfiguration
     {
+        /// <summary>
+        /// Address for the User Admin 2 - User API service.
+        /// </summary>
+        public string UserAdmin2ApiBaseAddress { get; set; }
+
+        /// <summary>
+        /// Decides whether User Admin 2 - User API should be used.
+        /// </summary>
+        public bool UseUserAdmin2Api { get; set; }
     }
 }
