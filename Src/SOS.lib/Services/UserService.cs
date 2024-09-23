56f33b889a218eaa98f57b1444b7929240e1a850
Martin Käck <martin.kack@slu.se>
2024-09-23 09:00:20 +0200

    Add support for UserAdmin2 in UserService

--- Src/SOS.lib/Services/UserService.cs
+++ Src/SOS.lib/Services/UserService.cs
@@ -46,6 +46,14 @@ namespace SOS.Lib.Services
         /// <inheritdoc />
         public async Task<UserModel> GetUserAsync()
         {
+            if (!_userServiceConfiguration.UseUserAdmin2Api)
+                return await GetUserFromUserAdmin1Async();
+            else
+                return await GetUserFromUserAdmin2Async();
+        }
+
+        private async Task<UserModel> GetUserFromUserAdmin1Async()
+        {
             try
             {
                 var authorizationHeader = _authorizationProvider.GetAuthHeader();
@@ -70,12 +78,47 @@ namespace SOS.Lib.Services
             return null;
         }
 
+        private async Task<UserModel> GetUserFromUserAdmin2Async()
+        {
+            try
+            {
+                var authorizationHeader = _authorizationProvider.GetAuthHeader();
+                if (string.IsNullOrEmpty(authorizationHeader))
+                {
+                    return null;
+                }
+
+                var response = await _httpClientService.GetDataAsync<ResponseModel<UserModel>>(
+                    new Uri($"{_userServiceConfiguration.UserAdmin2ApiBaseAddress}/Users/Current?artdataFormat=true"),
+                   new Dictionary<string, string> { { "Authorization", authorizationHeader } });
+
+                return response?.Success ?? false
+                    ? response.Result
+                    : throw new Exception(string.Concat(response?.Messages?.Select(m => m.Text)));
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "Failed to get user");
+            }
+
+            return null;
+        }
+
+
         /// <summary>
         /// Get user by id.
         /// </summary>
         /// <param name="userId"></param>
         /// <returns></returns>
         public async Task<UserModel> GetUserByIdAsync(int userId)
+        {
+            if (!_userServiceConfiguration.UseUserAdmin2Api)
+                return await GetUserByIdFromUserAdmin1Async(userId);
+            else
+                return await GetUserByIdFromUserAdmin2Async(userId);
+        }
+
+        private async Task<UserModel> GetUserByIdFromUserAdmin1Async(int userId)
         {
             try
             {
@@ -94,8 +137,36 @@ namespace SOS.Lib.Services
             return null;
         }
 
+        private async Task<UserModel> GetUserByIdFromUserAdmin2Async(int userId)
+        {
+            try
+            {
+                var response = await _httpClientService.GetDataAsync<ResponseModel<UserModel>>(
+                    new Uri($"{_userServiceConfiguration.UserAdmin2ApiBaseAddress}/Users/{userId}?artdataFormat=true"));
+
+                return response.Success
+                    ? response.Result
+                    : throw new Exception(string.Concat(response.Messages?.Select(m => m.Text)));
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "Failed to get user");
+            }
+
+            return null;
+        }
+
+
         /// <inheritdoc />
         public async Task<IEnumerable<AuthorityModel>> GetUserAuthoritiesAsync(int userId, string authorizationApplicationIdentifier = null, string cultureCode = "sv-SE")
+        {
+            if (!_userServiceConfiguration.UseUserAdmin2Api)
+                return await GetUserAuthoritiesFromUserAdmin1Async(userId);
+            else
+                return await GetUserAuthoritiesFromUserAdmin2Async(userId);
+        }
+
+        private async Task<IEnumerable<AuthorityModel>> GetUserAuthoritiesFromUserAdmin1Async(int userId, string authorizationApplicationIdentifier = null, string cultureCode = "sv-SE")
         {
             try
             {
@@ -114,8 +185,35 @@ namespace SOS.Lib.Services
             return null;
         }
 
+        private async Task<IEnumerable<AuthorityModel>> GetUserAuthoritiesFromUserAdmin2Async(int userId, string authorizationApplicationIdentifier = null, string cultureCode = "sv-SE")
+        {
+            try
+            {
+                var response = await _httpClientService.GetDataAsync<ResponseModel<IEnumerable<AuthorityModel>>>(
+                    new Uri($"{_userServiceConfiguration.UserAdmin2ApiBaseAddress}/Users/{userId}/permissions?applicationIdentifier={authorizationApplicationIdentifier ?? "artportalen"}&lang={cultureCode}&artdataFormat=true"));
+
+                return response.Success
+                    ? response.Result
+                    : throw new Exception(string.Concat(response?.Messages?.Select(m => m.Text)));
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "Failed to get user authorities");
+            }
+
+            return null;
+        }
+
         /// <inheritdoc />
         public async Task<IEnumerable<RoleModel>> GetUserRolesAsync(int userId, string authorizationApplicationIdentifier = null, string cultureCode = "sv-SE")
+        {
+            if (!_userServiceConfiguration.UseUserAdmin2Api)
+                return await GetUserRolesFromUserAdmin1Async(userId, authorizationApplicationIdentifier, cultureCode);
+            else
+                return await GetUserRolesFromUserAdmin2Async(userId, authorizationApplicationIdentifier, cultureCode);
+        }
+
+        private async Task<IEnumerable<RoleModel>> GetUserRolesFromUserAdmin1Async(int userId, string authorizationApplicationIdentifier = null, string cultureCode = "sv-SE")
         {
             try
             {
@@ -135,8 +233,55 @@ namespace SOS.Lib.Services
             return null;
         }
 
+        private async Task<IEnumerable<RoleModel>> GetUserRolesFromUserAdmin2Async(int userId, string authorizationApplicationIdentifier = null, string cultureCode = "sv-SE")
+        {
+            try
+            {
+                var cultureId = CultureCodeHelper.GetCultureId(cultureCode);
+                var response = await _httpClientService.GetDataAsync<ResponseModel<IEnumerable<RoleModel>>>(
+                    new Uri($"{_userServiceConfiguration.UserAdmin2ApiBaseAddress}/Users/{userId}/roles?applicationIdentifier={authorizationApplicationIdentifier ?? "artportalen"}&localeId={cultureId}&artdataFormat=true"));
+
+                return response?.Success ?? false
+                    ? response.Result
+                    : throw new Exception(string.Concat(response?.Messages?.Select(m => m.Text) ?? Array.Empty<string>()));
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "Failed to get user roles");
+            }
+
+            return null;
+        }
+
         /// <inheritdoc />
         public async Task<PersonModel> GetPersonAsync(int personId, string cultureCode = "sv-SE")
+        {
+            if (!_userServiceConfiguration.UseUserAdmin2Api)
+                return await GetPersonFromUserAdmin1Async(personId, cultureCode);
+            else
+                return await GetPersonFromUserAdmin2Async(personId, cultureCode);
+        }
+
+        private async Task<PersonModel> GetPersonFromUserAdmin1Async(int personId, string cultureCode = "sv-SE")
+        {
+            try
+            {
+                var response = await _httpClientService.GetDataAsync<ResponseModel<PersonModel>>(
+                    new Uri($"{_userServiceConfiguration.BaseAddress}/Person/{personId}?lang={cultureCode}"));
+
+                return response?.Success ?? false
+                    ? response.Result
+                    : throw new Exception(string.Concat(response?.Messages?.Select(m => m.Text) ?? Array.Empty<string>()));
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "Failed to get person information");
+            }
+
+            return null;
+        }
+
+        private async Task<PersonModel> GetPersonFromUserAdmin2Async(int personId, string cultureCode = "sv-SE")
         {
             try
             {
