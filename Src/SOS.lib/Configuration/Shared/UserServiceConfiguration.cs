5f5af531f6a19e09bb8a9d958d0be5cc6f27cd28
Martin Käck <martin.kack@slu.se>
2024-09-23 14:13:45 +0200

    Add client credentials settings to UserServiceConfiguration

--- Src/SOS.lib/Configuration/Shared/UserServiceConfiguration.cs
+++ Src/SOS.lib/Configuration/Shared/UserServiceConfiguration.cs
@@ -11,5 +11,20 @@
         /// Decides whether User Admin 2 - User API should be used.
         /// </summary>
         public bool UseUserAdmin2Api { get; set; }
+
+        /// <summary>
+        /// SOS client id.
+        /// </summary>
+        public string ClientId { get; set; }
+
+        /// <summary>
+        /// SOS client secret.
+        /// </summary>
+        public string ClientSecret { get; set; }
+
+        /// <summary>
+        /// Token URL.
+        /// </summary>
+        public string TokenUrl { get; set; }        
     }
 }
