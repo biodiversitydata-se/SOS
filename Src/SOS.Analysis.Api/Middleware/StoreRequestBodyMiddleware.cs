ef736461c293490f2b36777e117aa889be5c3899
Martin Käck <martin.kack@slu.se>
2025-02-22 16:26:11 +0100

    Fix request body logging size limit

--- Src/SOS.Analysis.Api/Middleware/StoreRequestBodyMiddleware.cs
+++ Src/SOS.Analysis.Api/Middleware/StoreRequestBodyMiddleware.cs
@@ -1,10 +1,14 @@
-﻿using System.Text;
+﻿using Nest;
+using SOS.Shared.Api.Dtos.Filter;
+using System.Text;
+using System.Text.Json;
 
 namespace SOS.Analysis.Api.Middleware;
 
 public class StoreRequestBodyMiddleware
 {
     private readonly RequestDelegate _next;
+    private const int ApplicationInsightsMaxSize = 8192;
 
     public StoreRequestBodyMiddleware(RequestDelegate next)
     {
@@ -26,6 +30,30 @@ public class StoreRequestBodyMiddleware
 
                 // Rewind, so the core is not lost when it looks the body for the request
                 context.Request.Body.Position = 0;
+
+                if (body != null && body.Length > ApplicationInsightsMaxSize)
+                {
+                    // truncate the body to the max size
+                    try
+                    {
+                        var searchFilter = JsonSerializer.Deserialize<SearchFilterInternalDto>(body);
+                        if (searchFilter?.Taxon?.Ids != null && searchFilter.Taxon.Ids.Any())
+                        {
+                            searchFilter.Taxon.Ids = [-1];
+                            body = JsonSerializer.Serialize(searchFilter);
+                        }
+
+                        if (body.Length > ApplicationInsightsMaxSize && searchFilter?.Geographics?.Geometries != null && searchFilter.Geographics.Geometries.Any())
+                        {
+                            searchFilter.Geographics.Geometries = [new PointGeoShape(new GeoCoordinate(-1, -1))];
+                            body = JsonSerializer.Serialize(searchFilter);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Deserialization failed: {ex.Message}");
+                    }
+                }
 
                 context.Items.Add("Request-body", body);
             }
--- Src/SOS.Observations.Api/Middleware/StoreRequestBodyMiddleware.cs
+++ Src/SOS.Observations.Api/Middleware/StoreRequestBodyMiddleware.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Nest;
+using SOS.Shared.Api.Dtos.Filter;
 using System;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace SOS.Observations.Api.Middleware
@@ -10,6 +13,7 @@ namespace SOS.Observations.Api.Middleware
     public class StoreRequestBodyMiddleware
     {
         private readonly RequestDelegate _next;
+        private const int ApplicationInsightsMaxSize = 8192;
 
         public StoreRequestBodyMiddleware(RequestDelegate next)
         {
@@ -27,10 +31,34 @@ namespace SOS.Observations.Api.Middleware
 
                     using var streamReader = new StreamReader(context.Request.Body, Encoding.UTF8, true, 1024, true);
 
-                    var body = await streamReader.ReadToEndAsync();
+                    string body = await streamReader.ReadToEndAsync();
 
                     // Rewind, so the core is not lost when it looks the body for the request
                     context.Request.Body.Position = 0;
+
+                    if (body != null && body.Length > ApplicationInsightsMaxSize)
+                    {
+                        // truncate the body to the max size
+                        try
+                        {
+                            var searchFilter = JsonSerializer.Deserialize<SearchFilterInternalDto>(body);
+                            if (searchFilter?.Taxon?.Ids != null && searchFilter.Taxon.Ids.Any())
+                            {                                
+                                searchFilter.Taxon.Ids = [-1];
+                                body = JsonSerializer.Serialize(searchFilter);
+                            }
+
+                            if (body.Length > ApplicationInsightsMaxSize && searchFilter?.Geographics?.Geometries != null && searchFilter.Geographics.Geometries.Any())
+                            {
+                                searchFilter.Geographics.Geometries = [ new PointGeoShape(new GeoCoordinate(-1, -1)) ];
+                                body = JsonSerializer.Serialize(searchFilter);
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine($"Deserialization failed: {ex.Message}");
+                        }
+                    }
 
                     context.Items.Add("Request-body", body);
                 }
