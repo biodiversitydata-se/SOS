7fd529df7b4d560f2321dcf881e2b133547507e7
Mats Lindgren <mats.lindgren@slu.se>
2025-05-21 15:01:49 +0200

    Removed disable health check init

--- Src/SOS.Analysis.Api/Startup.cs
+++ Src/SOS.Analysis.Api/Startup.cs
@@ -74,7 +74,6 @@ namespace SOS.Analysis.Api
         private const string PublicApiName = "PublicSosAnalysis";
         private const string InternalApiPrefix = "Internal";
         private bool _disableHangfireInit = false;
-        private bool _disableHealthCheckInit = false;
         private bool _useLocalHangfire = false;
         private bool _isDevelopment;
         private IWebHostEnvironment CurrentEnvironment { get; set; }
@@ -149,7 +148,6 @@ namespace SOS.Analysis.Api
                 builder.AddUserSecrets<Startup>();
             }
             _disableHangfireInit = GetEnvironmentBool(environmentVariable: "DISABLE_HANGFIRE_INIT");
-            _disableHealthCheckInit = GetEnvironmentBool(environmentVariable: "DISABLE_HEALTHCHECK_INIT");
             _useLocalHangfire = GetEnvironmentBool(environmentVariable: "USE_LOCAL_HANGFIRE");
 
             Configuration = builder.Build();
@@ -355,13 +353,10 @@ namespace SOS.Analysis.Api
             services.AddScoped<IProcessClient, ProcessClient>(p => new ProcessClient(processedSettings, processedDbConfiguration.DatabaseName,
                 processedDbConfiguration.ReadBatchSize, processedDbConfiguration.WriteBatchSize));
 
-            if (!_disableHealthCheckInit)
-            {
-                services.AddHealthChecks()
-                    .AddCheck<HealthCheck>("CustomHealthCheck", tags: ["k8s"])
-                    .AddCheck<AggregateHealthCheck>("AggregateHealthCheck", tags: ["Analysis.API"])
-                    .AddSystemMemory(1000, "System memory", tags: ["System"]);
-            }
+            services.AddHealthChecks()
+                .AddCheck<HealthCheck>("CustomHealthCheck", tags: ["k8s"])
+                .AddCheck<AggregateHealthCheck>("AggregateHealthCheck", tags: ["Analysis.API"])
+                .AddSystemMemory(1000, "System memory", tags: ["System"]);
 
             // Add configuration
             services.AddSingleton(analysisConfiguration!);
@@ -594,14 +589,11 @@ namespace SOS.Analysis.Api
             {
                 endpoints.MapControllers();
 
-                if (!_disableHealthCheckInit)
+                endpoints.MapHealthChecks("/healthz");
+                endpoints.MapHealthChecks("/health", new HealthCheckOptions()
                 {
-                    endpoints.MapHealthChecks("/healthz");
-                    endpoints.MapHealthChecks("/health", new HealthCheckOptions()
-                    {
-                        ResponseWriter = (context, _) => UIResponseWriter.WriteHealthCheckUIResponse(context, _)
-                    });
-                }
+                    ResponseWriter = (context, _) => UIResponseWriter.WriteHealthCheckUIResponse(context, _)
+                });
             });
         }
         
