caa08e814fafda0d137826fb19dee2c294665144
Mats Lindgren <mats.lindgren@slu.se>
2025-08-15 13:31:50 +0200

    Test AddDataProtection

--- Src/SOS.Administration.Api/Program.cs
+++ Src/SOS.Administration.Api/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.Mvc.Authorization;
@@ -125,6 +126,11 @@ static void ConfigureServices(
         options.KnownNetworks.Clear();
         options.KnownProxies.Clear();
     });
+
+    services.AddDataProtection()
+        .SetApplicationName("SOSAdminAPI");
+    //.PersistKeysToStackExchangeRedis(redisConnectionMultiplexer, "DataProtection-Keys")
+
 
     services.AddAuthentication(options =>
     {
