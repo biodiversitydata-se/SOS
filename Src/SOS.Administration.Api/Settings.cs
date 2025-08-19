f273b9b794a1cf54ea6ef42e1d2ccf1f7d05288e
Mats Lindgren <mats.lindgren@slu.se>
2025-08-19 13:22:42 +0200

    Config fix

--- Src/SOS.Administration.Api/Settings.cs
+++ Src/SOS.Administration.Api/Settings.cs
@@ -106,10 +106,9 @@ public static class Settings
         LocalHangfireDbConfiguration = GetConfigSection<HangfireDbConfiguration>("LocalHangfireDbConfiguration", configuration, logger, sensitiveSetting: false);
 
         RedisConfiguration = GetConfigSection<RedisConfiguration>("RedisConfiguration", configuration, logger, sensitiveSetting: false);
-        var redisPassword = GetConfigValueString("RedisPassword", configuration, logger, sensitiveSetting: true, required: false);
         if (RedisConfiguration.Password.Contains("SECRET_PLACEHOLDER", StringComparison.CurrentCultureIgnoreCase))
         {
-            RedisConfiguration.Password = redisPassword;
+            RedisConfiguration.Password = GetConfigValueString("RedisPassword", configuration, logger, sensitiveSetting: true, required: false);
             logger.LogInformation("replaced SECRET_PLACEHOLDER in RedisConfiguration.Password with the value in RedisPassword");
         }
         else
