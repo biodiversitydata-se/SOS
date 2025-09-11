1447f51f547f25eb8891a26e8d5eac81abe88fe6
Martin Käck <martin.kack@slu.se>
2025-09-11 13:06:09 +0200

    Add GeometrySchemaFilter

---
+++ Src/SOS.Lib/Swagger/PointSchemaFilter.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using Microsoft.OpenApi.Models;
+using NetTopologySuite.Geometries;
+using Microsoft.OpenApi.Any;
+
+namespace SOS.Lib.Swagger;
+
+public class PointSchemaFilter : ISchemaFilter
+{
+    public void Apply(OpenApiSchema schema, SchemaFilterContext context)
+    {
+        if (context.Type == typeof(Point))
+        {
+            schema.Type = "object";
+            schema.Required = new HashSet<string> { "type", "coordinates" };
+
+            schema.Properties = new Dictionary<string, OpenApiSchema>
+            {
+                ["type"] = new OpenApiSchema
+                {
+                    Type = "string",
+                    Enum = new List<IOpenApiAny>
+                    {
+                        new OpenApiString("Point"),                        
+                    }
+                },
+                ["coordinates"] = new OpenApiSchema
+                {
+                    Type = "array",
+                    Items = new OpenApiSchema { Type = "number" },
+                    MinItems = 2
+                }
+            };
+        }
+    }
+}
