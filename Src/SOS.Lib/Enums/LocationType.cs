ea71ba4d8e8a819c13b8242c464185bfd457f002
Martin Käck <martin.kack@slu.se>
2025-10-28 16:54:14 +0100

    Update LocationType

--- Src/SOS.Lib/Enums/LocationType.cs
+++ Src/SOS.Lib/Enums/LocationType.cs
@@ -3,12 +3,9 @@ namespace SOS.Lib.Enums
 {
     
     public enum LocationType
-    {
-        //  [EnumMember(Value = "Unknown")]
-        Unknown = 0,
-        //  [EnumMember(Value = "Point")]
-        Point,
-        //  [EnumMember(Value = "Polygon")]
-        Polygon
+    {        
+        Unknown = 0,        
+        Point = 1,        
+        Polygon = 2
     }
 }
