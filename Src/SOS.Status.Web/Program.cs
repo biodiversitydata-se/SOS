15c2a81c0086be693dafe239516506f62a37a9fd
Martin Käck <martin.kack@slu.se>
2025-11-13 19:32:22 +0100

    only use web assembly debug when DEBUG is defined

--- Src/SOS.Status.Web/Program.cs
+++ Src/SOS.Status.Web/Program.cs
@@ -99,12 +99,12 @@ try
     app.MapTaxonDiagramEndpoints();
     app.MapObservationSearchEndpoints();
 
-    // Configure the HTTP request pipeline.
-    if (isDevelopment)
-    {
-        app.UseWebAssemblyDebugging();                
-    }
-    else
+#if DEBUG
+    app.UseWebAssemblyDebugging();
+#endif
+    
+    // Configure the HTTP request pipeline.    
+    if (!isDevelopment)
     {
         app.UseExceptionHandler("/Error", createScopeForErrors: true);
         //The default HSTS value is 30 days.You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
